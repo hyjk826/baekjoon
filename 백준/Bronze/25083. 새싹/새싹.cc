fb700a9f35062ae8e7734be07f6e3b636fd1bceb
hyjk826 <84390446+hyjk826@users.noreply.github.com>
2022-06-28 03:49:06 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,24 @@
+// 2022-06-28
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define pi pair<int, int>
+#define vp vector<pi>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main(){
+    fastio;
+    cout << "         ,r'\"7\n";
+    cout << "r`-_   ,'  ,/\n";
+    cout << " \\. \". L_r'\n";
+    cout << "   `~\\/\n";
+    cout << "      |\n";
+    cout << "      |\n";
+}
