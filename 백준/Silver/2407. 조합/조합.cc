8faf1490eb3e8f489a16d4b64d6b8b16ab8b7620
hyjk826 <hyjk826@naver.com>
2022-07-16 23:15:04 +0900

    [Silver IV] Title: 조합, Time: 0 ms, Memory: 2440 KB -BaekjoonHub

---
+++ 백준/Silver/2407. 조합/조합.cc
@@ -0,0 +1,56 @@
+// 2022-07-16
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vs vector<string>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+vector<vs> dp(101, vs(101));
+string f(string s, string t){
+    int n = max((int)s.size(), (int)t.size());
+    if((int)s.size() > (int)t.size()){
+        t = string((int)s.size() - (int)t.size(), '0') + t;
+    }
+    else{
+        s = string((int)t.size() - (int)s.size(), '0') + s;
+    }
+    vi vec(n + 1);
+    for(int i{n}; i >= 1; --i){
+        vec[i] += s[i - 1] - '0' + t[i - 1] - '0';
+        if(vec[i] >= 10){
+            vec[i] -= 10;
+            vec[i - 1]++;
+        }
+    }
+    string r;
+    if(vec.front() != 0){
+        r += vec.front() + '0';
+    }
+    for(int i{1}; i <= n; ++i){
+        r += vec[i] + '0';
+    }
+    return r;
+}
+
+string DP(int n, int m){
+    if(dp[n][m].size() != 0) return dp[n][m];
+    if(n == m || m == 0) return dp[n][m] = '1';
+    return dp[n][m] = f(DP(n - 1, m - 1), DP(n - 1, m));
+}
+
+int main() {
+	fastio;
+    int n, m;
+    cin >> n >> m;
+    cout << DP(n, m);
+}
