05264ca6e877dff4c8416375d688ce69c52740b7
hyjk826 <hyjk826@naver.com>
2022-07-17 23:28:26 +0900

    [Platinum III] Title: XOR, Time: 536 ms, Memory: 19604 KB -BaekjoonHub

--- 백준/Platinum/12844. XOR/XOR.cc
+++ 백준/Platinum/12844. XOR/XOR.cc
@@ -1,35 +1,40 @@
-#include <iostream>
-#include <queue>
-#include <string>
-#include <stack>
-#include <algorithm>
-#include <cmath>
-#include <set>
-#include <map>
-#include <unordered_set>
+// 2022-07-17
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
 #define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
 #define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
 #define ll long long
 #define MAX 2147000000
-#define MOD 998244353
+#define MOD 1000000007
 using namespace std;
 
 struct st {
-	int v, lazy;
+	int value, lazy;
 };
 
 vector<st> segTree(2000001);
 vector<int> vec(500001);
 
-int init(int node, int s, int e) {
-	if (s == e) return segTree[node].v = vec[s];
-	int m{ (s + e) / 2 };
-	return segTree[node].v = init(node * 2, s, m) ^ init(node * 2 + 1, m + 1, e);
+void init(int node, int s, int e) {
+	if (s == e){
+        segTree[node].value = vec[s];
+        return;
+    }
+    int m = (s + e) / 2;
+	init(node * 2, s, m);
+    init(node * 2 + 1, m + 1, e);
+    segTree[node].value = segTree[node * 2].value ^ segTree[node * 2 + 1].value;
 }
 
 void update(int node, int s, int e, int l, int r, int v) {
 	if (segTree[node].lazy != 0) {
-		if ((e - s + 1) & 1) segTree[node].v ^= segTree[node].lazy;
+		if ((e - s + 1) & 1) segTree[node].value ^= segTree[node].lazy;
 		if (s != e) {
 			segTree[node * 2].lazy ^= segTree[node].lazy;
 			segTree[node * 2 + 1].lazy ^= segTree[node].lazy;
@@ -38,22 +43,23 @@ void update(int node, int s, int e, int l, int r, int v) {
 	}
 	if (r < s || e < l) return;
 	if (l <= s && e <= r) {
-		if ((e - s + 1) & 1) segTree[node].v ^= v;
+		if ((e - s + 1) & 1) segTree[node].value ^= v;
 		if (s != e) {
 			segTree[node * 2].lazy ^= v;
 			segTree[node * 2 + 1].lazy ^= v;
 		}
 		return;
 	}
+
 	int m{ (s + e) / 2 };
 	update(node * 2, s, m, l, r, v);
 	update(node * 2 + 1, m + 1, e, l, r, v);
-	segTree[node].v = segTree[node * 2].v ^ segTree[node * 2 + 1].v;
+	segTree[node].value = segTree[node * 2].value ^ segTree[node * 2 + 1].value;
 }
 
 int f(int node, int s, int e, int l, int r) {
 	if (segTree[node].lazy != 0) {
-		if ((e - s + 1) & 1) segTree[node].v ^= segTree[node].lazy;
+		if ((e - s + 1) & 1) segTree[node].value ^= segTree[node].lazy;
 		if (s != e) {
 			segTree[node * 2].lazy ^= segTree[node].lazy;
 			segTree[node * 2 + 1].lazy ^= segTree[node].lazy;
@@ -61,32 +67,29 @@ int f(int node, int s, int e, int l, int r) {
 		segTree[node].lazy = 0;
 	}
 	if (r < s || e < l) return 0;
-	if (l <= s && e <= r) return segTree[node].v;
+	if(l <= s && e <= r) return segTree[node].value;
 	int m{ (s + e) / 2 };
 	return f(node * 2, s, m, l, r) ^ f(node * 2 + 1, m + 1, e, l, r);
 }
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	int n;
+	fastio;
+	int n, m;
 	cin >> n;
 	for (int i{ 0 }; i < n; ++i) {
 		cin >> vec[i];
 	}
 	init(1, 0, n - 1);
-	int t;
-	cin >> t;
-	while (t--) {
+	cin >> m;
+	while (m--) {
 		int a, b, c, d;
-		cin >> a >> b >> c;
+		cin >> a;
 		if (a == 1) {
-			cin >> d;
-			if (b > c) swap(b, c);
+			cin >> b >> c >> d;
 			update(1, 0, n - 1, b, c, d);
 		}
 		else if (a == 2) {
-			if (b > c) swap(b, c);
+			cin >> b >> c;
 			cout << f(1, 0, n - 1, b, c) << "\n";
 		}
 	}
