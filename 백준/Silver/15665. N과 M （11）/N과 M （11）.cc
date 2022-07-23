476acb5d1361bcb68c392cf5a74ea2685c1b02c2
hyjk826 <hyjk826@naver.com>
2022-07-24 02:06:37 +0900

    [Silver II] Title: N과 M (11), Time: 412 ms, Memory: 65248 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.cc
@@ -0,0 +1,52 @@
+// 2022-07-23
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int n, k;
+vi vec;
+vector<vi> ans;
+
+void dfs(int L, vi& v){
+    if(L == k){
+        ans.push_back(v);
+    }
+    else{
+        for(auto& i : vec){
+            v.push_back(i);
+            dfs(L + 1, v);
+            v.pop_back();
+        }
+    }
+}
+
+int main() {
+	fastio;
+    cin >> n >> k;
+    vec.resize(n);
+    for(int i{0}; i < n; ++i){
+        cin >> vec[i];
+    }
+    vi v;
+    dfs(0, v);
+    sort(ans.begin(), ans.end());
+    ans.erase(unique(ans.begin(), ans.end()), ans.end());
+    for(auto& i : ans){
+        for(auto& j : i){
+            cout << j << " ";
+        }
+        cout << "\n";
+    }
+}
+
