4513233a83b4d89eab1461c83ed27f8a8e4d644a
hyjk826 <hyjk826@naver.com>
2022-07-26 10:03:49 +0900

    [Silver V] Title: 올림픽, Time: 0 ms, Memory: 2152 KB -BaekjoonHub

---
+++ 백준/Silver/8979. 올림픽/올림픽.cc
@@ -0,0 +1,41 @@
+// 2022-07-26
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+
+int main() {
+	fastio;
+    int n, k;
+    cin >> n >> k;
+    vi A(n + 1), B(n + 1), C(n + 1);
+    for(int i{0}; i < n; ++i){
+        int index;
+        cin >> index;
+        cin >> A[index] >> B[index] >> C[index];
+    }   
+    int ans{0}; 
+    for(int i{1}; i <= n; ++i){
+        if(A[i] > A[k]) ans++;
+        else if(A[i] == A[k]){
+            if(B[i] > B[k]) ans++;
+            else if(B[i] == B[k]){
+                if(C[i] > C[k]) ans++;
+            }
+        }
+    }
+    cout << ans + 1;
+}
+
