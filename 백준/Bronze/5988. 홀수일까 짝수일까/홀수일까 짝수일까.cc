1b13b37e7b55a6f28b7ec839fe9ee095d5b39a90
hyjk826 <hyjk826@naver.com>
2022-07-26 11:57:54 +0900

    [Bronze III] Title: 홀수일까 짝수일까, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/5988. 홀수일까 짝수일까/홀수일까 짝수일까.cc
@@ -0,0 +1,30 @@
+// 2022-07-26
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main() {
+	fastio;
+    int n;
+    cin >> n;
+    while(n--){
+        string str;
+        cin >> str;
+        int a = str.back() - '0';
+        if(a & 1) cout << "odd\n";
+        else cout << "even\n";
+    }
+}
+
