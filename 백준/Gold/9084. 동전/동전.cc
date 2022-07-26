9e8f7ff567acdc1c8ebc361cc0b136646db6f08f
hyjk826 <hyjk826@naver.com>
2022-07-26 11:07:51 +0900

    [Gold V] Title: 동전, Time: 0 ms, Memory: 2176 KB -BaekjoonHub

---
+++ 백준/Gold/9084. 동전/동전.cc
@@ -0,0 +1,41 @@
+// 2022-07-26
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main() {
+	fastio;
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        vi vec(n);
+        for(int i{0}; i < n; ++i){
+            cin >> vec[i];
+        }
+        int k;
+        cin >> k;
+        vi dp(k + 1);
+        dp[0] = 1;
+        for(int i{0}; i < n; ++i){
+            for(int j{vec[i]}; j <= k; ++j){
+                dp[j] += dp[j - vec[i]];
+            }
+        }
+        cout << dp[k] << "\n";
+    }
+}
+
