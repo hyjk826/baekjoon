098a38a153e01600cc0b6a65ca0f1c66caffd634
hyjk826 <hyjk826@naver.com>
2022-07-28 05:53:06 +0900

    [Bronze III] Title: 첫 글자를 대문자로, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/4458. 첫 글자를 대문자로/첫 글자를 대문자로.cc
@@ -0,0 +1,30 @@
+// 2022-07-28
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main() {
+	fastio;
+    int t;
+    cin >> t;
+    string str;
+    getline(cin, str);
+    for(int i{0}; i < t; ++i){        
+        getline(cin, str);
+        str[0] = toupper(str[0]);
+        cout << str << "\n";
+    }
+}
+
