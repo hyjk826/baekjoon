79edd3a07fc8a9d73bf392137118d0612a2a2fef
hyjk826 <hyjk826@naver.com>
2022-07-28 08:13:12 +0900

    [Bronze III] Title: 더하기 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11023. 더하기 3/더하기 3.cc
@@ -0,0 +1,25 @@
+// 2022-07-28
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main() {
+	fastio;
+    int ans{0};
+    int k;
+    while(cin >> k) ans += k;
+    cout << ans;
+}
+
