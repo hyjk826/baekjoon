f66decbfe4f78b7c2dcba6eee0caa115f2af0f90
hyjk826 <hyjk826@naver.com>
2022-07-28 23:14:54 +0900

    [Bronze III] Title: 꼬리를 무는 숫자 나열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1598. 꼬리를 무는 숫자 나열/꼬리를 무는 숫자 나열.cc
@@ -0,0 +1,26 @@
+// 2022-07-28
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main() {
+	fastio;
+    int a, b;
+    cin >> a >> b;
+    a--;
+    b--;
+    cout << abs(a / 4 - b / 4) + abs(a % 4 - b % 4);
+}
+
