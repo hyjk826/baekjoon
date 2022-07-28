865585cacf5a69852137636629bf8e2e1fda9f5f
hyjk826 <hyjk826@naver.com>
2022-07-28 23:16:18 +0900

    [Bronze IV] Title: 치킨 두 마리 (...), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14489. 치킨 두 마리 （．．．）/치킨 두 마리 （．．．）.cc
@@ -0,0 +1,25 @@
+// 2022-07-28
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main() {
+	fastio;
+    int a, b, c;
+    cin >> a >> b >> c;
+    if(a + b >= 2 * c) cout << a + b - 2 * c;
+    else cout << a + b;
+}
+
