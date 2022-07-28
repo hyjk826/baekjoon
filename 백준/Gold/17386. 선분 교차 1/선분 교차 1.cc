2dd1822db1dd6c9eeb444c534f729d338b8a8c7f
hyjk826 <hyjk826@naver.com>
2022-07-29 04:23:30 +0900

    [Gold III] Title: 선분 교차 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/17386. 선분 교차 1/선분 교차 1.cc
@@ -0,0 +1,54 @@
+// 2022-07-29
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int ccw(pl a, pl b, pl c){
+    ll op = a.first * b.second + b.first * c.second + c.first * a.second;
+    op -= (a.second * b.first + b.second * c.first + c.second * a.first);
+    if(op > 0) return 1;
+    else if(op == 0) return 0;
+    else return -1;
+}
+
+bool isIntersect(pair<pl, pl> x, pair<pl, pl> y){
+    pl a = x.first;
+    pl b = x.second;
+    pl c = y.first;
+    pl d = y.second;
+    ll ab = ccw(a, b, c) * ccw(a, b, d);
+    ll cd = ccw(c, d, a) * ccw(c, d, b);
+    if(ab == 0 && cd == 0){
+        if(a > b) swap(a, b);
+        if(c > d) swap(c, d);
+        return c <= b && a <= d;
+    }
+    return ab <= 0 && cd <= 0;
+}
+
+
+int main() {
+	fastio;
+    vl A(4), B(4);
+    for(int i{0}; i < 4; ++i) cin >> A[i];
+    for(int i{0}; i < 4; ++i) cin >> B[i];
+    pl a = {A[0], A[1]};
+    pl b = {A[2], A[3]};
+    pl c = {B[0], B[1]};
+    pl d = {B[2], B[3]};
+    if(isIntersect({a, b}, {c, d})) cout << 1;
+    else cout << 0;
+}
+
