3482ee6e77c9b762964bdeea87be4501bd39cb8b
hyjk826 <hyjk826@naver.com>
2022-07-29 12:08:49 +0900

    [Platinum V] Title: Counting Inversions, Time: 152 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Platinum/10090. Counting Inversions/Counting Inversions.cc
@@ -0,0 +1,59 @@
+// 2022-07-29
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+template <typename T>
+struct binary_indexed_tree{
+  int N;
+  vector<T> BIT;
+  binary_indexed_tree(int N): N(N), BIT(N + 1, 0){
+  }
+  void add(int i, T x){
+    i++;
+    while (i <= N){
+      BIT[i] += x;
+      i += i & -i;
+    }
+  }
+  T sum(int i){
+    T ans = 0;
+    while (i > 0){
+      ans += BIT[i];
+      i -= i & -i;
+    }
+    return ans;
+  }
+  T f(int a, int b){
+    if(a == 0) return sum(b + 1);
+    else return sum(b + 1) - sum(a);
+  }
+};
+
+int main() {
+	fastio;
+    int n;
+    cin >> n;
+    binary_indexed_tree<int> BIT(n + 1);
+    ll ans{0};
+    for(int i{0}; i < n; ++i){
+        int a;
+        cin >> a;
+        ans += BIT.f(a + 1, n);
+        BIT.add(a, 1);
+    }
+    cout << ans;
+}
+
