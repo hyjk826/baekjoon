f53a11d9bfe9038a814f445ef726085f7e38a0d4
hyjk826 <hyjk826@naver.com>
2022-07-31 19:28:26 +0900

    [Bronze II] Title: 알파벳 거리, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/5218. 알파벳 거리/알파벳 거리.cc
@@ -0,0 +1,37 @@
+// 2022-07-31
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main() {
+	fastio;
+    int t;
+    cin >> t;
+    while(t--){
+        string s, t;
+        cin >> s >> t;
+        cout << "Distances: ";
+        for(int i{0}; i < (int)s.size(); ++i){
+            if(t[i] >= s[i]){
+                cout << t[i] - s[i] << " ";
+            }
+            else{
+                cout << t[i] + 26 - s[i] << " ";
+            }
+        }
+        cout << "\n";
+    }
+}
+
