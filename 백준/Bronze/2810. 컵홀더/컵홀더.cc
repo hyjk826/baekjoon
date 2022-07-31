c697248b36fd474ab5540944133547d2fb24e16b
hyjk826 <hyjk826@naver.com>
2022-07-31 16:20:33 +0900

    [Bronze I] Title: 컵홀더, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2810. 컵홀더/컵홀더.cc
@@ -0,0 +1,31 @@
+// 2022-07-31
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main() {
+	fastio;
+    int n;
+    cin >> n;
+    string str;
+    cin >> str;
+    int cnt{1};
+    for(int i{0}; i < n; ++i){
+        cnt++;
+        if(str[i]== 'L') i++;
+    }
+    cout << min(n, cnt);
+}
+
