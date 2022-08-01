72b43349e79feae80dbedf82bed93a180950dbad
hyjk826 <hyjk826@naver.com>
2022-08-01 11:20:30 +0900

    [Bronze II] Title: 더하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10822. 더하기/더하기.cc
@@ -0,0 +1,36 @@
+// 2022-07-31
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main() {
+	fastio;
+    string str;
+    cin >> str;
+    str += ',';
+    string a;
+    int ans{0};
+    for(int i{0}; i < (int)str.size(); ++i){
+        if(str[i] == ','){
+            ans += stoi(a);
+            a = "";
+        }
+        else{
+            a += str[i];
+        }
+    }
+    cout << ans;
+}
+
