226d2298107de0125ad4407ed60428fca44cfc59
hyjk826 <hyjk826@naver.com>
2022-08-14 20:07:46 +0900

    [Bronze II] Title: JOI와 IOI, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/5586. JOI와 IOI/JOI와 IOI.cc
@@ -0,0 +1,33 @@
+// 2022-08-14
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main() {
+	fastio;
+    string str;
+    cin >> str;
+    int a{0}, b{0};
+    for(int i{1}; i < (int)str.size() - 1; ++i){
+        if(str[i] == 'O'){
+            if(str[i + 1] == 'I'){
+                if(str[i - 1] == 'J') a++;
+                else if(str[i - 1] == 'I') b++;
+            }
+        }
+    }
+    cout << a << "\n" << b;
+}
+
