d7679b4e812a6c50928c5129bf48b75ca34cfee7
hyjk826 <hyjk826@naver.com>
2022-08-14 20:54:08 +0900

    [Bronze III] Title: 히스토그램, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/13752. 히스토그램/히스토그램.cc
@@ -0,0 +1,28 @@
+// 2022-08-14
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main() {
+	fastio;
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        cout << string(n, '=') << "\n";
+    }
+}
+
