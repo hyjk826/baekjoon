8733960d1326e899ab634c7758394cc5b1167aa7
hyjk826 <hyjk826@naver.com>
2022-08-15 17:30:17 +0900

    [Silver I] Title: 물병, Time: 32 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1052. 물병/물병.cc
@@ -0,0 +1,29 @@
+// 2022-08-15
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main() {
+	fastio;
+    int n, k;
+    cin >> n >> k;
+    for(int i{0};;++i){
+        int a = n + i;
+        if(__builtin_popcount(a) <= k){
+            cout << i;
+            return 0;
+        }
+    }
+}
