72f06655966ba07453423ae3f1844b3f51d3dc72
hyjk826 <hyjk826@naver.com>
2022-08-15 16:59:19 +0900

    [Gold V] Title: 무한 수열, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Gold/1351. 무한 수열/무한 수열.cc
@@ -0,0 +1,33 @@
+// 2022-08-15
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+ll p, q;
+map<ll, ll> mp;
+
+ll f(ll n){
+    if(mp.count(n)) return mp[n];    
+    return mp[n] = f(n / p) + f(n / q);
+}
+
+
+int main() {
+	fastio;
+    mp[0] = 1;
+    ll n;
+    cin >> n >> p >> q;
+    cout << f(n);
+}
