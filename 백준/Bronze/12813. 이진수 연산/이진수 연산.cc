098bb883d0f30dc2929daaf47fe937a3b7fed7a8
hyjk826 <hyjk826@naver.com>
2022-08-15 16:20:50 +0900

    [Bronze II] Title: 이진수 연산, Time: 4 ms, Memory: 2244 KB -BaekjoonHub

---
+++ 백준/Bronze/12813. 이진수 연산/이진수 연산.cc
@@ -0,0 +1,23 @@
+// 2022-08-15
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main() {
+	fastio;
+    bitset<100000> a, b;
+    cin >> a >> b;
+    cout << (a & b) << "\n" << (a | b) << "\n" << (a ^ b) << "\n" << ~a << "\n" << ~b;
+}
