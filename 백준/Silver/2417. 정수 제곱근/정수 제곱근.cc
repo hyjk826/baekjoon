9bdec8f9d6a2c2c0d4cdea592d32fd7513fc6f3a
hyjk826 <hyjk826@naver.com>
2022-08-19 12:49:59 +0900

    [Silver IV] Title: 정수 제곱근, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/2417. 정수 제곱근/정수 제곱근.cc
@@ -0,0 +1,26 @@
+// 2022-08-19
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main() {
+	fastio;
+    ll n;
+    cin >> n;
+    ll a = sqrt(n);
+    if(a * a >= n) cout << a;
+    else cout << a + 1;
+}
+
