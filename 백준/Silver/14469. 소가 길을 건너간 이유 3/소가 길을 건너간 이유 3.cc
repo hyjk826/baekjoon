057bfbb6b34445c9d5938c8dddced2e0bef2802a
hyjk826 <hyjk826@naver.com>
2022-08-19 22:12:10 +0900

    [Silver IV] Title: 소가 길을 건너간 이유 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/14469. 소가 길을 건너간 이유 3/소가 길을 건너간 이유 3.cc
@@ -0,0 +1,32 @@
+// 2022-08-19
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define vp vector<pi>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main(){
+    fastio;
+	int n;
+    cin >> n;
+    vp vec(n);
+    for(int i{0}; i < n; ++i){
+        cin >> vec[i].first >> vec[i].second;
+    }
+    sort(vec.begin(), vec.end());
+    int ans{0};
+    for(int i{0}; i < n; ++i){
+        if(ans >= vec[i].first) ans += vec[i].second;
+        else ans = vec[i].first + vec[i].second;
+    }
+    cout << ans;
+}
