ba71d71ab7b9448e310f8e182b1003aa9424a8d3
hyjk826 <hyjk826@naver.com>
2022-08-19 21:35:52 +0900

    [Silver IV] Title: 가로수, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/2485. 가로수/가로수.cc
@@ -0,0 +1,37 @@
+// 2022-08-19
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define vp vector<pi>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int gcd(int a, int b){
+    if(b == 0) return a;
+    return gcd(b, a % b);
+}
+
+int main(){
+    fastio;
+    int n;
+    cin >> n;
+    vi vec(n);
+    for(int i{0}; i < n; ++i){
+        cin >> vec[i];
+    }
+    int g{0};
+    sort(vec.begin(), vec.end());
+    for(int i{0}; i < n - 1; ++i){
+        g = gcd(g, vec[i + 1] - vec[i]);
+    }
+    int k = (vec.back() - vec.front()) / g;
+    cout << k + 1 - n;
+}
