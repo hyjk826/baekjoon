ba3692a18e8363e5f9599b915ca1a160c69024c8
hyjk826 <hyjk826@naver.com>
2022-08-21 10:38:14 +0900

    [Silver III] Title: 안테나, Time: 32 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Silver/18310. 안테나/안테나.cc
@@ -0,0 +1,28 @@
+// 2022-08-21
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define vp vector<pi>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main(){
+    fastio;
+	int n;
+    cin >> n;
+    vi vec(n);
+    for(int i{0}; i < n; ++i){
+        cin >> vec[i];
+    }
+    sort(vec.begin(), vec.end());
+    int k = vec[(n - 1) / 2];
+    cout << k;
+}
