f79d6f5b35469720bcf9f5f52ae36ba7d87c0648
hyjk826 <hyjk826@naver.com>
2022-08-23 03:29:54 +0900

    [Bronze II] Title: 간지(干支), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7572. 간지（干支）/간지（干支）.cc
@@ -0,0 +1,27 @@
+// 2022-08-23
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main() {
+	fastio;
+    int n;
+    cin >> n;
+    n += 12000 - 2013; 
+    int a = (n + 9) % 10;
+    int b = (n + 5) % 12;
+    cout << char('A' + b) << a;
+}
+
