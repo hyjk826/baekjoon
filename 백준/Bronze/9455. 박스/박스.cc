4294149700b2c0adad776f12f542e932234d601d
hyjk826 <hyjk826@naver.com>
2022-08-22 22:39:01 +0900

    [Bronze I] Title: 박스, Time: 24 ms, Memory: 2152 KB -BaekjoonHub

---
+++ 백준/Bronze/9455. 박스/박스.cc
@@ -0,0 +1,47 @@
+// 2022-08-22
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main() {
+	fastio;
+    int t;
+    cin >> t;
+    while(t--){
+        int n, m;
+        cin >> n >> m;
+        vector<vi> board(n, vi(m));
+        for(int i{0}; i < n; ++i){
+            for(int j{0}; j < m; ++j){
+                cin >> board[i][j];
+            }
+        }
+        int ans{0};
+        for(int j{0}; j < m; ++j){
+            for(int i{n - 1}; i >= 0; --i){
+                if(board[i][j] == 1){
+                    board[i][j] = 0;
+                    int x = i;
+                    while(x + 1 < n && board[x + 1][j] == 0){
+                        x++;
+                    }
+                    ans += x - i;
+                    board[x][j] = 1;
+                }
+            }
+        }
+        cout << ans << "\n";
+    }
+}
