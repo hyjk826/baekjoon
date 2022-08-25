a88bb1e32e55acb9d8b76663bac3b7e79064efc6
hyjk826 <hyjk826@naver.com>
2022-08-26 03:23:29 +0900

    [Gold II] Title: 컵라면, Time: 64 ms, Memory: 5252 KB -BaekjoonHub

---
+++ 백준/Gold/1781. 컵라면/컵라면.cc
@@ -0,0 +1,42 @@
+// 2022-08-25
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main() {
+	fastio;
+    int n;
+    cin >> n;
+    vp vec(n);
+    for(int i{0}; i < n; ++i){
+        cin >> vec[i].first >> vec[i].second;
+    }
+    sort(vec.rbegin(), vec.rend());
+    int k{0};
+    priority_queue<int> pQ;
+    ll ans{0};
+    for(int i{n}; i >= 1; --i){
+        while(k < n && vec[k].first >= i){
+            pQ.push(vec[k].second);
+            k++;
+        }
+        if(!pQ.empty()){
+            ans += pQ.top();
+            pQ.pop();
+        }
+    }
+    cout << ans;
+}
+
