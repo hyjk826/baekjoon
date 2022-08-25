cf090c69b8f24aad949fce1ea460f8ffb3292e0b
hyjk826 <hyjk826@naver.com>
2022-08-25 20:49:04 +0900

    [Bronze III] Title: 2의 제곱인가?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11966. 2의 제곱인가？/2의 제곱인가？.cc
@@ -0,0 +1,25 @@
+// 2022-08-23
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main() {
+	fastio;
+    int n;
+    cin >> n;
+    if(__builtin_popcount(n) == 1) cout << 1;
+    else cout << 0;
+}
+
