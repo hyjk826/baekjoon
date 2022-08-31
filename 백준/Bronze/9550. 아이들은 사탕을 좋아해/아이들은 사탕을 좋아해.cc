cd8af76b2f93265ecda10198c44cb129d25625ce
hyjk826 <hyjk826@naver.com>
2022-09-01 00:32:18 +0900

    [Bronze III] Title: 아이들은 사탕을 좋아해, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9550. 아이들은 사탕을 좋아해/아이들은 사탕을 좋아해.cc
@@ -0,0 +1,34 @@
+// 2022-09-01
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main() {
+	fastio;
+    int t;
+    cin >> t;
+    while(t--){
+        int n, k;
+        cin >> n >> k;
+        int ans{0};
+        for(int i{0}; i < n; ++i){
+            int a;
+            cin >> a;
+            ans += a / k;
+        }
+        cout << ans << "\n";
+    }
+}
+
