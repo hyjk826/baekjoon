24ebbbf0256e6e575df9f45dc3b10ef4fd3adfb1
hyjk826 <hyjk826@naver.com>
2022-09-01 18:03:02 +0900

    [Bronze III] Title: 우유 축제, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14720. 우유 축제/우유 축제.cc
@@ -0,0 +1,34 @@
+// 2022-09-01
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main() {
+	fastio;
+    int n;
+    cin >> n;
+    int ans{0};
+    int k{0};
+    for(int i{0}; i < n; ++i){
+        int a;
+        cin >> a;
+        if(a == k){
+            ans++;
+            k = (k + 1) % 3;
+        }
+    }
+    cout << ans;
+}
+
