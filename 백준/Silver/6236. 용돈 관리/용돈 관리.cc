277fe5556bc495be52b7472f60236757d1c6dc03
hyjk826 <hyjk826@naver.com>
2022-09-03 22:57:20 +0900

    [Silver II] Title: 용돈 관리, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/6236. 용돈 관리/용돈 관리.cc
+++ 백준/Silver/6236. 용돈 관리/용돈 관리.cc
@@ -20,28 +20,25 @@ int main() {
     int n, k;
     cin >> n >> k;
     vi vec(n);
+    int mx{0};
     for(int i{0}; i < n; ++i){
         cin >> vec[i];
+        mx = max(mx, vec[i]);
     }
-    int l{1}, r{MAX};
+    int l{mx}, r{MAX};
     int ans{MAX};
     while(l <= r){
         int m{(l + r) / 2};
         int sum = m;
         int cnt{1};
-        bool flag = true;
         for(int i{0}; i < n; ++i){
-            if(vec[i] > m){
-                flag = false;
-                break;
-            }
             if(sum < vec[i]){
                 sum = m;
                 cnt++;
             }
             sum -= vec[i];
         }
-        if(flag && cnt <= k){
+        if(cnt <= k){
             ans = m;
             r = m - 1;
         }
