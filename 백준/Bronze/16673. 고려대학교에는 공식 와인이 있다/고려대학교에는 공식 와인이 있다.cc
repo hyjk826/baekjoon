14994a26419a10fff7c86fb657b6040102819e24
hyjk826 <hyjk826@naver.com>
2022-09-05 01:35:28 +0900

    [Bronze III] Title: 고려대학교에는 공식 와인이 있다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16673. 고려대학교에는 공식 와인이 있다/고려대학교에는 공식 와인이 있다.cc
@@ -0,0 +1,29 @@
+// 2022-09-05
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main() {
+	fastio;
+    int n, k, p;
+    cin >> n >> k >> p;
+    int ans{0};
+    for(int i{1}; i <= n; ++i){
+        ans += i * k + p * i * i;
+    }
+    cout << ans;
+}
+	
+
