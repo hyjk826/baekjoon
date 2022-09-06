98653c240bbf5107012cf1ea40948a56be539bdd
hyjk826 <hyjk826@naver.com>
2022-09-06 16:57:05 +0900

    [Gold V] Title: 평범한 배낭, Time: 16 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,4 +1,4 @@
-// 2022-05-29
+// 2022-09-06
 #include <bits/stdc++.h>
 #define fastio                    \
 	ios_base::sync_with_stdio(0); \
@@ -6,24 +6,34 @@
 #define vi vector<int>
 #define vl vector<long long>
 #define vc vector<char>
+#define vs vector<string>
 #define pi pair<int, int>
+#define pl pair<ll, ll>
 #define vp vector<pi>
 #define ll long long
 #define MAX 2147000000
 #define MOD 1000000007
 using namespace std;
 
-int main(){
-    fastio;
-	int n, k;
-	cin >> n >> k;
-	vector<int> dp(k + 1);
-	for (int i{ 0 }; i < n; ++i) {
-		int w, v;
-		cin >> w >> v;
-		for (int j{ k }; j >= w; --j) {
-			dp[j] = max(dp[j], dp[j - w] + v);
-		}
-	}
-	cout << dp[k];
+int main() {
+	fastio;
+    int n, k;
+    cin >> n >> k;
+    vi dp(k + 1, -MAX);
+    dp[0] = 0;
+    for(int i{0}; i < n; ++i){
+        int a, b;
+        cin >> a >> b;
+        for(int j{k - a}; j >= 0; --j){
+            if(dp[j] == -1) continue;
+            dp[j + a] = max(dp[j + a], dp[j] + b);
+        }
+    }
+    int ans{0};
+    for(int i{0}; i <= k; ++i){
+        ans = max(ans, dp[i]);
+    }
+    cout << ans;
 }
+	
+
