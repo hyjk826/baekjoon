2de9a804b42821ecafb8cdd4ad8235af3d30bf65
hyjk826 <hyjk826@naver.com>
2022-09-09 03:44:46 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.cc
@@ -0,0 +1,23 @@
+// 2022-09-09
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define vp vector<pi>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main(){
+    fastio;
+	int n;
+    cin >> n;
+    if(n % 4 == 0 && (!(n % 100 == 0) || (n % 400 == 0))) cout << 1;
+    else cout << 0;
+}
