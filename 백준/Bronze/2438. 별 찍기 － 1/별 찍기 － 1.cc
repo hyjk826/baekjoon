f24588938c31c547faebc727715aadbfc3764024
hyjk826 <hyjk826@naver.com>
2022-09-09 03:36:53 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -0,0 +1,24 @@
+// 2022-09-09
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define vp vector<pi>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main(){
+    fastio;
+	int n;
+    cin >> n;
+    for(int i{0}; i < n; ++i){
+        cout << string(i + 1, '*') << "\n";
+    }
+}
