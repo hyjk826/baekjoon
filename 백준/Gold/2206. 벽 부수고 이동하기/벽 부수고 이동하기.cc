b2650315ad75be599b5e1dd4b0520a38f3065c31
hyjk826 <hyjk826@naver.com>
2022-09-08 12:42:39 +0900

    [Gold IV] Title: 벽 부수고 이동하기, Time: 80 ms, Memory: 12624 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.cc
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.cc
@@ -1,59 +1,73 @@
-#include <iostream>
-#include <vector>
-#include <queue>
+// 2022-09-08
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
 using namespace std;
 
-struct st {
-	int x, y, state, cnt;
+struct st{
+    int x, y, t;
 };
-bool ch[2][1000][1000]{};
 
-int xx[]{ -1,0,1,0 };
-int yy[]{ 0,1,0,-1 };
+int ch[1000][1000][2]{};
 
 int main() {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	int n, m;
-	cin >> n >> m;
-	vector<vector<int> > map(n, vector<int>(m));
-	for (int i{ 0 }; i < n; ++i) {
-		for (int j{ 0 }; j < m; ++j) {
-			char a;
-			cin >> a;
-			map[i][j] = a - '0';
-		}
-	}
-	queue<st> Q;
-	Q.push({ 0,0,0,1 });
-	ch[0][0][0] = 1;
-	while (!Q.empty()) {
-		int x{ Q.front().x };
-		int y{ Q.front().y };
-		int state{ Q.front().state };
-		int cnt{ Q.front().cnt };
-		Q.pop();
-		if (x == n - 1 && y == m - 1) {
-			cout << cnt;
-			return 0;
-		}
-		for (int i{ 0 }; i < 4; ++i) {
-			int nx{ x + xx[i] };
-			int ny{ y + yy[i] };
-			if (nx < 0 || nx > n - 1 || ny < 0 || ny > m - 1) continue;
-			if (map[nx][ny] == 0) {
-				if (ch[state][nx][ny] == 0) {
-					ch[state][nx][ny] = 1;
-					Q.push({ nx, ny, state, cnt + 1 });
-				}
-			}
-			else {
-				if (state == 0 && ch[1][nx][ny] == 0) {
-					ch[1][nx][ny] = 1;
-					Q.push({ nx,ny, 1, cnt + 1 });
-				}
-			}
-		}
-	}
-	cout << -1;
+	fastio;
+    int n, m;
+    cin >> n >> m;
+    vector<vc> board(n, vc(m));
+    for(int i{0}; i < n; ++i){
+        for(int j{0}; j < m; ++j){
+            cin >> board[i][j];
+            for(int k{0}; k < 2; ++k){
+                ch[i][j][k] = -1;
+            }
+        }
+    }
+    queue<st> Q;
+    Q.push({0, 0, 1});
+    ch[0][0][1] = 1;
+    int xx[]{-1,0,1,0};
+    int yy[]{0,1,0,-1};
+    while(!Q.empty()){
+        int x{Q.front().x};
+        int y{Q.front().y};
+        int t{Q.front().t};
+        if(x == n - 1 && y == m - 1){
+            cout << ch[x][y][t];
+            return 0;
+        }
+        Q.pop();
+        for(int dir{0}; dir < 4; ++dir){
+            int nx{x + xx[dir]};
+            int ny{y + yy[dir]};
+            if(nx < 0 || nx > n - 1 || ny < 0 || ny > m - 1) continue;
+            if(board[nx][ny] == '0'){
+                if(ch[nx][ny][t] == -1){
+                    ch[nx][ny][t] = ch[x][y][t] + 1;
+                    Q.push({nx, ny, t});
+                }
+            }
+            else{
+                if(t == 0) continue;
+                if(ch[nx][ny][0] == -1){
+                    ch[nx][ny][0] = ch[x][y][t] + 1;
+                    Q.push({nx, ny, 0});
+                }
+            }
+        }
+    }
+    cout << -1;
 }
+	
+
