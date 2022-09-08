5c75c0f563dbbc6a6f4145c2b1415bcf38c1ef20
hyjk826 <hyjk826@naver.com>
2022-09-09 03:20:28 +0900

    [Bronze II] Title: 단어의 개수, Time: 12 ms, Memory: 5180 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.cc
@@ -0,0 +1,26 @@
+// 2022-09-09
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define vp vector<pi>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main(){
+    fastio;
+	string str;
+    getline(cin, str);
+    stringstream ss(str);
+    string t;
+    int ans{0};
+    while(ss >> t) ans++;
+    cout << ans;
+}
