66f58b0cfc7a22aade45786cf8d88cfd5c12cede
hyjk826 <hyjk826@naver.com>
2022-09-10 03:24:00 +0900

    [Silver I] Title: 단지번호붙이기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
@@ -1,4 +1,4 @@
-// 2022-09-06
+// 2022-09-10
 #include <bits/stdc++.h>
 #define fastio                    \
 	ios_base::sync_with_stdio(0); \
@@ -8,15 +8,17 @@
 #define vc vector<char>
 #define vs vector<string>
 #define pi pair<int, int>
-#define pl pair<ll, ll>
 #define vp vector<pi>
 #define ll long long
 #define MAX 2147000000
 #define MOD 1000000007
 using namespace std;
 
-int main() {
-	fastio;
+int xx[]{-1,0,1,0};
+int yy[]{0,1,0,-1};
+
+int main(){
+    fastio;
     int n;
     cin >> n;
     vector<vc> board(n, vc(n));
@@ -26,15 +28,13 @@ int main() {
         }
     }
     vi ans;
-    int xx[]{-1,0,1,0};
-    int yy[]{0,1,0,-1};
     for(int i{0}; i < n; ++i){
         for(int j{0}; j < n; ++j){
             if(board[i][j] == '1'){
-                int cnt{0};
-                board[i][j] = '0';
                 queue<pi> Q;
                 Q.push({i, j});
+                board[i][j] = '0';
+                int cnt{0};
                 while(!Q.empty()){
                     int x{Q.front().first};
                     int y{Q.front().second};
@@ -54,9 +54,7 @@ int main() {
             }
         }
     }
-    cout << ans.size() << "\n";
     sort(ans.begin(), ans.end());
+    cout << ans.size() << "\n";
     for(auto& i : ans) cout << i << "\n";
 }
-	
-
