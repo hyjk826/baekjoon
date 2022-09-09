144f3b492ae661143ab306448b17ea7bfa91dbee
hyjk826 <hyjk826@naver.com>
2022-09-09 15:45:24 +0900

    [Silver IV] Title: 수 찾기, Time: 100 ms, Memory: 6776 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -0,0 +1,34 @@
+// 2022-09-09
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define vp vector<pi>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main(){
+    fastio;
+    int n, m;
+    cin >> n;
+    set<int> st;
+    for(int i{0}; i < n; ++i){
+        int a;
+        cin >> a;
+        st.insert(a);
+    }
+    cin >> m;
+    for(int i{0}; i < m; ++i){
+        int a;
+        cin >> a;
+        if(st.count(a)) cout << 1 << "\n";
+        else cout << 0 << "\n";
+    }
+}
