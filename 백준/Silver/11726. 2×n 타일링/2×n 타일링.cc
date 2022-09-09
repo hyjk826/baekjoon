f9739df0f498324f6e51b4622ab535cd9c8c0537
hyjk826 <hyjk826@naver.com>
2022-09-10 03:25:49 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,29 @@
+// 2022-09-10
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define vp vector<pi>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main(){
+    fastio;
+    int n;
+    cin >> n;
+    vi dp(1001);
+    dp[0] = 1;
+    dp[1] = 1;
+    for(int i{2}; i <= 1000; ++i){
+        dp[i] = dp[i - 1] + dp[i - 2];
+        dp[i] %= 10007;
+    }
+    cout << dp[n];
+}
