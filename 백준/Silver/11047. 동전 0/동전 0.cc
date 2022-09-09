135eba2c6cd755f6f4df7f19f53cc84d356f2729
hyjk826 <hyjk826@naver.com>
2022-09-10 02:56:29 +0900

    [Silver IV] Title: 동전 0, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.cc
@@ -0,0 +1,31 @@
+// 2022-09-10
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define vp vector<pi>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main(){
+    fastio;
+    int n, k;
+    cin >> n >> k;
+    vi vec(n);
+    for(int i{0}; i < n; ++i){
+        cin >> vec[i];
+    }
+    int ans{0};
+    for(int i{n - 1}; i >= 0; --i){
+        ans += k / vec[i];
+        k %= vec[i];
+    }
+    cout << ans;
+}
