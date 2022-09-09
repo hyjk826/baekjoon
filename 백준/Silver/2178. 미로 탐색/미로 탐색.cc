964f70c62b370b093fbcff31751362727d374c60
hyjk826 <hyjk826@naver.com>
2022-09-10 03:15:47 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -0,0 +1,46 @@
+// 2022-09-10
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define vp vector<pi>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main(){
+    fastio;
+    int n, m;
+    cin >> n >> m;
+    vector<vc> board(n, vc(m));
+    for(int i{0}; i < n; ++i){
+        for(int j{0}; j < m; ++j){
+            cin >> board[i][j];
+        }
+    }
+    vector<vi> ch(n, vi(m, -1));
+    ch[0][0] = 0;
+    queue<pi> Q;
+    Q.push({0, 0});
+    int xx[]{-1,0,1,0};
+    int yy[]{0,1,0,-1};
+    while(!Q.empty()){
+        int x{Q.front().first};
+        int y{Q.front().second};
+        Q.pop();
+        for(int dir{0}; dir < 4; ++dir){
+            int nx{x + xx[dir]};
+            int ny{y + yy[dir]};
+            if(nx < 0 || nx > n - 1 || ny < 0 || ny > m - 1 || board[nx][ny] == '0' || ch[nx][ny] != -1) continue;
+            ch[nx][ny] = ch[x][y] + 1;
+            Q.push({nx, ny});
+        }
+    }
+    cout << ch[n - 1][m - 1] + 1;
+}
