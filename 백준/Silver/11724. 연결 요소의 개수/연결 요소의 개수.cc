c7badf642fac176855eb9c53137a9c3b1882340d
hyjk826 <hyjk826@naver.com>
2022-09-10 16:53:38 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 88 ms, Memory: 6524 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.cc
@@ -0,0 +1,49 @@
+// 2022-09-10
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define vp vector<pi>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main(){
+    fastio;
+    int n, m;
+    cin >> n >> m;
+    vector<vi> g(n + 1);
+    for(int i{0}; i < m; ++i){
+        int a, b;
+        cin >> a >> b;
+        g[a].push_back(b);
+        g[b].push_back(a);
+    }
+    vi ch(n + 1);
+    int ans{0};
+    for(int i{1}; i <= n; ++i){
+        if(ch[i] == 0){
+            ans++;
+            queue<int> Q;
+            Q.push(i);
+            ch[i] = 1;
+            while(!Q.empty()){
+                int x{Q.front()};
+                Q.pop();
+                for(auto& j : g[x]){
+                    if(ch[j] == 0){
+                        ch[j] = 1;
+                        Q.push(j);
+                    }
+                }
+            }
+        }
+    }
+    cout << ans;
+}
