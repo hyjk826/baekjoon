fe6844a3a76c65d477b087dc4c3cb9b2d7f68806
hyjk826 <hyjk826@naver.com>
2022-09-12 00:31:24 +0900

    [Silver I] Title: 트리 순회, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.cc
+++ 백준/Silver/1991. 트리 순회/트리 순회.cc
@@ -1,4 +1,4 @@
-// 2022-09-06
+// 2022-09-11
 #include <bits/stdc++.h>
 #define fastio                    \
 	ios_base::sync_with_stdio(0); \
@@ -8,49 +8,49 @@
 #define vc vector<char>
 #define vs vector<string>
 #define pi pair<int, int>
-#define pl pair<ll, ll>
 #define vp vector<pi>
 #define ll long long
 #define MAX 2147000000
 #define MOD 1000000007
 using namespace std;
 
-vector<int> vec[26];
+int n;
+vector<vi> g(26);
 
-void dfs1(int k) {
-	if (k == '.' - 'A') return;
-	cout << char(k + 'A');
-	dfs1(vec[k][0]);
-    dfs1(vec[k][1]);
-}
-void dfs2(int k) {
-	if (k == '.' - 'A') return;
-	dfs2(vec[k][0]);
-	cout << char(k + 'A');
-	dfs2(vec[k][1]);
+void preorder(int k){
+    if(k == '.' - 'A') return;
+    cout << char('A' + k);
+    for(auto& i : g[k]){
+        preorder(i);
+    }
 }
 
-void dfs3(int k) {
-	if (k == '.' - 'A') return;
-	dfs3(vec[k][0]);
-    dfs3(vec[k][1]);
-	cout << char(k + 'A');
+void inorder(int k){
+    if(k == '.' - 'A') return;
+    inorder(g[k][0]);
+    cout << char('A' + k);
+    inorder(g[k][1]);
 }
 
-int main() {
-	fastio;
-	int n;
-	cin >> n;
-	for (int i{ 0 }; i < n; ++i) {
-		char a, b, c;
-		cin >> a >> b >> c;
-		vec[a - 'A'].push_back(b - 'A');
-		vec[a - 'A'].push_back(c - 'A');
-	}
-	dfs1(0);
-	cout << "\n";
-	dfs2(0);
-	cout << "\n";
-	dfs3(0);
+void postorder(int k){
+    if(k == '.' - 'A') return;
+    postorder(g[k][0]);
+    postorder(g[k][1]);
+    cout << char('A' + k);
 } 
 
+int main(){
+    fastio;
+    cin >> n;
+    for(int i{0}; i < n; ++i){
+        char a, b, c;
+        cin >> a >> b >> c;
+        g[a - 'A'].push_back(b - 'A');
+        g[a - 'A'].push_back(c - 'A');
+    }
+    preorder(0);
+    cout << "\n";
+    inorder(0);
+    cout << "\n";
+    postorder(0);
+}
