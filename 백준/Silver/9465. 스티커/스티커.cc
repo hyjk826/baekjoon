ea91f042d01dad48e28d82ae8835b6cc4bfbf147
hyjk826 <hyjk826@naver.com>
2022-09-12 00:47:13 +0900

    [Silver I] Title: 스티커, Time: 92 ms, Memory: 4156 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.cc
+++ 백준/Silver/9465. 스티커/스티커.cc
@@ -1,4 +1,4 @@
-// 2022-09-06
+// 2022-09-11
 #include <bits/stdc++.h>
 #define fastio                    \
 	ios_base::sync_with_stdio(0); \
@@ -8,15 +8,14 @@
 #define vc vector<char>
 #define vs vector<string>
 #define pi pair<int, int>
-#define pl pair<ll, ll>
 #define vp vector<pi>
 #define ll long long
 #define MAX 2147000000
 #define MOD 1000000007
 using namespace std;
 
-int main() {
-	fastio;
+int main(){
+    fastio;
     int t;
     cin >> t;
     while(t--){
@@ -28,19 +27,16 @@ int main() {
                 cin >> board[i][j];
             }
         }
-        if(n == 1){
-            cout << max(board[0][0], board[1][0]) << "\n";
-            continue;
-        }
-        board[0][1] += board[1][0];
-        board[1][1] += board[0][0];
+        vector<vi> dp(2, vi(n));
+        dp[0][0] = board[0][0];
+        dp[1][0] = board[1][0];
+        dp[0][1] = board[0][1] + board[1][0];
+        dp[1][1] = board[1][1] + board[0][0];
         for(int j{2}; j < n; ++j){
             for(int i{0}; i < 2; ++i){
-                board[i][j] += max({board[0][j - 2], board[1][j - 2], board[i ^ 1][j - 1]});
+                dp[i][j] = max({dp[i ^ 1][j - 1], dp[0][j - 2], dp[1][j - 2]}) + board[i][j];
             }
         }
-        cout << max(board[0][n - 1], board[1][n - 1]) << "\n";
+        cout << max(dp[0][n - 1], dp[1][n - 1]) << "\n";
     }
 }
-	
-
