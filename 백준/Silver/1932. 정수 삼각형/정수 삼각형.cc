3e690f570c3cb1599defcce30ffefd63031da1b2
hyjk826 <hyjk826@naver.com>
2022-09-11 23:26:25 +0900

    [Silver I] Title: 정수 삼각형, Time: 12 ms, Memory: 3080 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
@@ -1,4 +1,4 @@
-// 2022-09-06
+// 2022-09-11
 #include <bits/stdc++.h>
 #define fastio                    \
 	ios_base::sync_with_stdio(0); \
@@ -8,15 +8,14 @@
 #define vc vector<char>
 #define vs vector<string>
 #define pi pair<int, int>
-#define pl pair<ll, ll>
 #define vp vector<pi>
 #define ll long long
 #define MAX 2147000000
 #define MOD 1000000007
 using namespace std;
 
-int main() {
-	fastio;
+int main(){
+    fastio;
     int n;
     cin >> n;
     vector<vi> board(n, vi(n));
@@ -25,26 +24,10 @@ int main() {
             cin >> board[i][j];
         }
     }
-    vector<vi> dp(n, vi(n));
-    dp[0][0] = board[0][0];
-    for(int i{1}; i < n; ++i){
+    for(int i{n - 2}; i >= 0; --i){
         for(int j{0}; j < i + 1; ++j){
-            if(j == 0){
-                dp[i][j] = dp[i - 1][0] + board[i][j];
-            }
-            else if(j == i){
-                dp[i][j] = dp[i - 1][j - 1] + board[i][j];
-            }
-            else{
-                dp[i][j] = max(dp[i - 1][j - 1], dp[i - 1][j]) + board[i][j];
-            }
+            board[i][j] += max(board[i + 1][j], board[i + 1][j + 1]);
         }
     }
-    int ans{0};
-    for(int i{0}; i < n; ++i){
-        ans = max(ans, dp[n - 1][i]);
-    }
-    cout << ans;
+    cout << board[0][0];
 }
-	
-
