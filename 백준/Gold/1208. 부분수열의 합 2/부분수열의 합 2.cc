0d2126be6612ca6da1db471555107d90ad6b0e63
hyjk826 <hyjk826@naver.com>
2022-09-14 02:21:32 +0900

    [Gold I] Title: 부분수열의 합 2, Time: 872 ms, Memory: 43868 KB -BaekjoonHub

--- 백준/Gold/1208. 부분수열의 합 2/부분수열의 합 2.cc
+++ 백준/Gold/1208. 부분수열의 합 2/부분수열의 합 2.cc
@@ -1,4 +1,4 @@
-// 2022-06-22
+// 2022-09-14
 #include <bits/stdc++.h>
 #define fastio                    \
 	ios_base::sync_with_stdio(0); \
@@ -6,41 +6,49 @@
 #define vi vector<int>
 #define vl vector<long long>
 #define vc vector<char>
+#define vs vector<string>
 #define pi pair<int, int>
+#define pl pair<ll, ll>
 #define vp vector<pi>
 #define ll long long
 #define MAX 2147000000
 #define MOD 1000000007
 using namespace std;
 
-int main(){
-    fastio;
-	int n, k;
-    cin >> n >> k;
+int main() {
+	fastio;
+    int n, s;
+    cin >> n >> s;
     vi vec(n);
-    for(int i{0}; i < n; ++i){
-        cin >> vec[i];
-    }
-    map<int, int> A;
-    for(int i{0}; i < (1 << (n / 2)); ++i){
+    for(int i{0}; i < n; ++i) cin >> vec[i];
+    int h = n / 2;
+    map<int, int> mp1, mp2;
+    for(int i{0}; i < (1 << h); ++i){
         int sum{0};
-        for(int j{0}; j < n / 2; ++j){
+        for(int j{0}; j < h; ++j){
             if((i >> j) & 1){
                 sum += vec[j];
             }
         }
-        A[sum]++;
+        mp1[sum]++;
     }
-    ll ans{0};
-    for(int i{0}; i < (1 << (n - n / 2)); ++i){
+    for(int i{0}; i < (1 << (n - h)); ++i){
         int sum{0};
-        for(int j{0}; j < n - n / 2; ++j){
+        for(int j{0}; j < n - h; ++j){
             if((i >> j) & 1){
-                sum += vec[n / 2 + j];
+                sum += vec[j + h];
             }
         }
-        ans += A[k - sum];
+        mp2[sum]++;
     }
-    if(k == 0) ans--;
+    ll ans{0};
+    for(auto& i : mp1){
+        if(mp2.count(s - i.first)){
+            ans += 1LL * i.second * mp2[s - i.first];
+        }
+    }
+    if(s == 0) ans--;
     cout << ans;
 }
+	
+
