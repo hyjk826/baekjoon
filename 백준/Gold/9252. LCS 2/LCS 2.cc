2b5a46702a7c939ce3f416afaf3be17ea708806b
hyjk826 <hyjk826@naver.com>
2022-09-14 02:07:56 +0900

    [Gold IV] Title: LCS 2, Time: 4 ms, Memory: 6004 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.cc
+++ 백준/Gold/9252. LCS 2/LCS 2.cc
@@ -1,4 +1,4 @@
-// 2022-06-23
+// 2022-09-14
 #include <bits/stdc++.h>
 #define fastio                    \
 	ios_base::sync_with_stdio(0); \
@@ -6,45 +6,50 @@
 #define vi vector<int>
 #define vl vector<long long>
 #define vc vector<char>
+#define vs vector<string>
 #define pi pair<int, int>
+#define pl pair<ll, ll>
 #define vp vector<pi>
 #define ll long long
 #define MAX 2147000000
 #define MOD 1000000007
 using namespace std;
 
-int main(){
-    fastio;
+int main() {
+	fastio;
     string s, t;
     cin >> s >> t;
-    vector<vi> dp((int)s.size() + 1, vi(t.size() + 1));
-    for(int i{1}; i <= (int)s.size(); ++i){
-        for(int j{1}; j <= (int)t.size(); ++j){
+    int n = (int)s.size();
+    int m = (int)t.size();
+    vector<vi> board(n + 1, vi(m + 1));
+    for(int i{1}; i <= n; ++i){
+        for(int j{1}; j <= m; ++j){
             if(s[i - 1] == t[j - 1]){
-                dp[i][j] = dp[i - 1][j - 1] + 1;
+                board[i][j] = board[i - 1][j - 1] + 1;
             }
             else{
-                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                board[i][j] = max(board[i - 1][j], board[i][j - 1]);
             }
         }
     }
-    pi p = {(int)s.size(), (int)t.size()};
     string ans;
+    int x = n;
+    int y = m;
     while(1){
-        if(p.first == 0 || p.second == 0) break;
-        if(s[p.first - 1] == t[p.second - 1]){
-            ans += s[p.first - 1];
-            p.first--;
-            p.second--;
+        if(s[x - 1] == t[y - 1]){
+            ans += s[x - 1];
+            x--;
+            y--;        
         }
         else{
-            if(dp[p.first][p.second] == dp[p.first - 1][p.second]){
-                p.first--;
-            }
-            else p.second--;
+            if(board[x - 1][y] == board[x][y]) x--;
+            else y--;
         }
+        if(x == 0 || y == 0) break;
     }
+    cout << board[n][m] << "\n";
     reverse(ans.begin(), ans.end());
-    cout << dp[(int)s.size()][(int)t.size()] << "\n";
     cout << ans;
 }
+	
+
