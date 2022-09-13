36a2149dac1d74b56a7c71a6dec33021e2fa99de
hyjk826 <hyjk826@naver.com>
2022-09-13 19:48:56 +0900

    [Gold IV] Title: 행렬 제곱, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Gold/10830. 행렬 제곱/행렬 제곱.cc
@@ -0,0 +1,65 @@
+// 2022-09-13
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int n;
+ll k;
+
+vector<vi> f(vector<vi> a, vector<vi>b) {
+    vector<vi> ans(n, vi(n));
+    for (int i{ 0 }; i < n; ++i) {
+        for (int j{ 0 }; j < n; ++j) {
+            for (int k{ 0 }; k < n; ++k) {
+                ans[i][j] += a[i][k] * b[k][j];
+                ans[i][j] %= 1000;
+            }
+        }
+    }
+    return ans;
+}
+
+vector<vi> binpow(vector<vi> a, ll b) {
+    vector<vi> ans(n, vi(n));
+    for (int i{ 0 }; i < n; ++i) {
+        ans[i][i] = 1;
+    }
+    if (b == 0) return ans;
+    auto k = binpow(a, b / 2);
+    if (b % 2) {
+        return f(f(k, k), a);
+    }
+    else {
+        return f(k, k);
+    }
+}
+
+int main() {
+    fastio;
+    cin >> n >> k;
+    vector<vi> A(n, vi(n));
+    for (int i{ 0 }; i < n; ++i) {
+        for (int j{ 0 }; j < n; ++j) {
+            cin >> A[i][j];
+        }
+    }
+    auto ans = binpow(A, k);
+    for (int i{ 0 }; i < n; ++i) {
+        for (int j{ 0 }; j < n; ++j) {
+            cout << ans[i][j] << " ";
+        }
+        cout << "\n";
+    }
+}
