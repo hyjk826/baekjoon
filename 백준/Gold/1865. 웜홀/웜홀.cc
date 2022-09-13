ca9c8b9fa920649330da084948fce2783f9532ca
hyjk826 <hyjk826@naver.com>
2022-09-13 21:33:13 +0900

    [Gold III] Title: 웜홀, Time: 24 ms, Memory: 2332 KB -BaekjoonHub

--- 백준/Gold/1865. 웜홀/웜홀.cc
+++ 백준/Gold/1865. 웜홀/웜홀.cc
@@ -1,32 +1,27 @@
-#include <iostream>
-#include <queue>
-#include <string>
-#include <stack>
-#include <map>
-#include <algorithm>
-#include <set>
-#include <cmath>
-#include <unordered_set>
-#include <map>
-#include <string>
-#include <cstring>
-#include <unordered_map>
-#include <sstream>
-#define fastio ios_base::sync_with_stdio(0); cin.tie(0);
+// 2022-09-13
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
 #define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
 #define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
 #define ll long long
-#define MAX 2147000000
-#define MOD 998244353LL
+#define MAX 1000000000
+#define MOD 1000000007
 using namespace std;
 
-struct st {
-    int v, e, dis;
+struct st{
+    int v, u, dis;
 };
 
 
 int main() {
-    fastio;
+	fastio;
     int t;
     cin >> t;
     while(t--){
@@ -36,36 +31,35 @@ int main() {
         for(int i{0}; i < m; ++i){
             int a, b, c;
             cin >> a >> b >> c;
-            a--;
-            b--;
             edge.push_back({a, b, c});
             edge.push_back({b, a, c});
         }
         for(int i{0}; i < w; ++i){
             int a, b, c;
             cin >> a >> b >> c;
-            a--;
-            b--;
             edge.push_back({a, b, -c});
         }
-        vi bell(n, MAX);
-        bell[0] = 0;
-        for(int i{0}; i < n - 1; ++i){
-            for(int j{0}; j < 2 * m + w; ++j){
-                int a = edge[j].v;
-                int b = edge[j].e;
-                int c = edge[j].dis;
-                bell[b] = min(bell[b], bell[a] + c);
+        vi dis(n + 1, MAX);
+        dis[1] = 0;
+        bool f = false;
+        for(int i{0}; i < n; ++i){
+            bool flag = false;
+            for(int j{0}; j < (int)edge.size(); ++j){
+                int v = edge[j].v;
+                int u = edge[j].u;
+                int d = edge[j].dis;
+                if(dis[v] + d < dis[u]){
+                    dis[u] = dis[v] + d;
+                    flag = true;
+                }
+            }
+            if(i == n - 1 && flag){
+                f = true;
             }
         }
-        bool flag = false;
-        for(int i{0}; i < 2 * m + w; ++i){
-            int a = edge[i].v;
-            int b = edge[i].e;
-            int c = edge[i].dis;
-            if(bell[b] > bell[a] + c) flag = true;
-        }
-        if(flag) cout << "YES\n";
+        if(f) cout << "YES\n";
         else cout << "NO\n";
-    }  
+    }
 }
+	
+
