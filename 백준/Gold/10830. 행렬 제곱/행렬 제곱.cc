12a20e003957c1eaa284d287e8e9716137587dc6
hyjk826 <hyjk826@naver.com>
2022-09-13 19:49:58 +0900

    [Gold IV] Title: 행렬 제곱, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Gold/10830. 행렬 제곱/행렬 제곱.cc
+++ 백준/Gold/10830. 행렬 제곱/행렬 제곱.cc
@@ -1,18 +1,13 @@
-// 2022-09-13
+// 2022-01-13
 #include <bits/stdc++.h>
 #define fastio                    \
-	ios_base::sync_with_stdio(0); \
-	cin.tie(0);
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);
 #define vi vector<int>
-#define vl vector<long long>
-#define vc vector<char>
-#define vs vector<string>
 #define pi pair<int, int>
-#define pl pair<ll, ll>
-#define vp vector<pi>
 #define ll long long
 #define MAX 2147000000
-#define MOD 1000000007
+#define MOD 998244353LL
 using namespace std;
 
 int n;
