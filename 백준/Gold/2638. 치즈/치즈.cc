955ac7048d9bc591d4e8bdc14148bb8b2a3b76ff
hyjk826 <hyjk826@naver.com>
2022-09-13 20:22:36 +0900

    [Gold III] Title: 치즈, Time: 16 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Gold/2638. 치즈/치즈.cc
@@ -0,0 +1,74 @@
+// 2022-09-13
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main(){
+    fastio;
+    int n, m;
+    cin >> n >> m;
+    vector<vi> board(n, vi(m));
+    for (int i{ 0 }; i < n; ++i) {
+        for (int j{ 0 }; j < m; ++j) {
+            cin >> board[i][j];
+        }
+    }  
+    int xx[]{ -1,0,1,0 };
+    int yy[]{ 0,1,0,-1 };
+    int ans{ 0 };
+    board[0][0] = 2;
+    while (1) {
+        queue<pi> Q;
+        Q.push({ 0,0 });
+        vector<vi> ch(n, vi(m));
+        ch[0][0] = 1;
+        while (!Q.empty()) {
+            int x{ Q.front().first };
+            int y{ Q.front().second };
+            Q.pop();
+            for (int i{ 0 }; i < 4; ++i) {
+                int nx{ x + xx[i] };
+                int ny{ y + yy[i] };
+                if (nx < 0 || nx > n - 1 || ny < 0 || ny > m - 1) continue;
+                if (ch[nx][ny] == 0 && board[nx][ny] != 1) {
+                    board[nx][ny] = 2;
+                    ch[nx][ny] = 1;
+                    Q.push({ nx,ny });
+                }
+            }
+        }
+        vector<pi> vec;
+        for (int i{ 0 }; i < n; ++i) {
+            for (int j{ 0 }; j < m; ++j) {
+                if (board[i][j] == 1) {
+                    int cnt{ 0 };
+                    for (int k{ 0 }; k < 4; ++k) {
+                        int nx = i + xx[k];
+                        int ny = j + yy[k];
+                        if (board[nx][ny] == 2) cnt++;
+                    }
+                    if (cnt >= 2) vec.push_back({ i,j });
+                }
+            }
+        }
+        for (auto& i : vec) {
+            board[i.first][i.second] = 2;
+        }
+        ans++;
+        if (vec.size() == 0) break;
+    }
+    cout << ans - 1;
+}
+
