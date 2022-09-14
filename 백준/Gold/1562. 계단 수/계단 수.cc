edf9b655c8e2a6b826967dced7ad93aa9c08c000
hyjk826 <hyjk826@naver.com>
2022-09-14 18:37:08 +0900

    [Gold I] Title: 계단 수, Time: 8 ms, Memory: 6060 KB -BaekjoonHub

--- 백준/Gold/1562. 계단 수/계단 수.cc
+++ 백준/Gold/1562. 계단 수/계단 수.cc
@@ -1,4 +1,4 @@
-// 2022-06-22
+// 2022-09-14
 #include <bits/stdc++.h>
 #define fastio                    \
 	ios_base::sync_with_stdio(0); \
@@ -6,41 +6,45 @@
 #define vi vector<int>
 #define vl vector<long long>
 #define vc vector<char>
+#define vs vector<string>
 #define pi pair<int, int>
+#define pl pair<ll, ll>
 #define vp vector<pi>
 #define ll long long
 #define MAX 2147000000
 #define MOD 1000000000
 using namespace std;
 
-int dp[101][10][1024]{};
+int dp[101][10][1 << 10]{};
 
-int main(){
-    fastio;
-	int n;
+
+int main() {
+	fastio;
+    int n;
     cin >> n;
-    for(int i{1}; i < 10; ++i){
-        dp[1][i][1 << i] = 1;
+    for(int i{1}; i <= 9; ++i){
+        dp[1][i][0 | (1 << i)] = 1;
     }
     for(int i{2}; i <= n; ++i){
-        for(int j{0}; j < 10; ++j){
-            for(int k{0}; k < 1024; ++k){
-                if(j == 0){
-                    dp[i][j][k | (1 << j)] = (dp[i][j][k | (1 << j)] + dp[i - 1][j + 1][k]) % MOD;    
-                }
-                else if(j == 9){
-                    dp[i][j][k | (1 << j)] = (dp[i][j][k | (1 << j)] + dp[i - 1][j - 1][k]) % MOD;    
-                }
-                else{
-                    dp[i][j][k | (1 << j)] = (dp[i][j][k | (1 << j)] + dp[i - 1][j - 1][k]) % MOD;
-                    dp[i][j][k | (1 << j)] = (dp[i][j][k | (1 << j)] + dp[i - 1][j + 1][k]) % MOD;
+        for(int j{0}; j <= 9; ++j){
+            int a;
+            for(int k{0}; k < 2; ++k){
+                if(k == 0) a = j - 1;
+                else a = j + 1;
+                if(a < 0 || a > 9) continue;
+                for(int l{0}; l < (1 << 10); ++l){
+                    dp[i][j][l | (1 << j)] += dp[i - 1][a][l];
+                    dp[i][j][l | (1 << j)] %= MOD;
                 }
             }
         }
     }
     int ans{0};
     for(int i{0}; i < 10; ++i){
-        ans = (ans + dp[n][i][1023]) % MOD;
+        ans += dp[n][i][(1 << 10) - 1];
+        ans %= MOD;
     }
     cout << ans;
 }
+	
+
