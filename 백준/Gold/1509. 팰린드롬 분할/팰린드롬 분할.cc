2dced51f911502c4219e49aae616874c8d9c4ce9
hyjk826 <hyjk826@naver.com>
2022-09-15 00:26:33 +0900

    [Gold I] Title: 팰린드롬 분할, Time: 684 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Gold/1509. 팰린드롬 분할/팰린드롬 분할.cc
+++ 백준/Gold/1509. 팰린드롬 분할/팰린드롬 분할.cc
@@ -1,4 +1,4 @@
-// 2022-06-22
+// 2022-09-14
 #include <bits/stdc++.h>
 #define fastio                    \
 	ios_base::sync_with_stdio(0); \
@@ -6,40 +6,39 @@
 #define vi vector<int>
 #define vl vector<long long>
 #define vc vector<char>
+#define vs vector<string>
 #define pi pair<int, int>
+#define pl pair<ll, ll>
 #define vp vector<pi>
 #define ll long long
 #define MAX 2147000000
 #define MOD 1000000007
 using namespace std;
 
-bool f(int a, int b, string& str, vector<vi>& dp) {
-    if (a > (int)str.size() - 1 || b < 0) return 1;
-    if (dp[a][b] != -1) return dp[a][b];
-    if (a > b) return dp[a][b] = 1;
-    if (str[a] == str[b]) return dp[a][b] = f(a + 1, b - 1, str, dp);
-    else return dp[a][b] = 0;
+string str;
+
+bool f(int l, int r){
+    if(l > r) return 1;
+    if(str[l] == str[r]) return f(l + 1, r - 1);
+    else return 0;
 }
 
 int main() {
-    fastio;
-    string str;
+	fastio;
     cin >> str;
     int n = (int)str.size();
-    vi ans(n, MAX);
-    vector<vi> dp(n, vi(n, -1));
-    for (int i{ 0 }; i < n; ++i) {
-        for (int j{ i }; j < n; ++j) {
-            dp[i][j] = f(i, j, str, dp);
-        }
-    }
-    for (int i{ 0 }; i < n; ++i) {
-        for (int j{ 0 }; j <= i; ++j) {
-            if (dp[j][i]) {
-                if (j == 0) ans[i] = min(ans[i], 1);
-                else ans[i] = min(ans[i], ans[j - 1] + 1);
+    vi dp(n, MAX);
+    for(int i{0}; i < n; ++i){
+        for(int j{0}; j <= i; ++j){
+            if(f(j, i)){
+                if(j == 0) dp[i] = 1;
+                else{
+                    dp[i] = min(dp[i], dp[j - 1] + 1);
+                }
             }
         }
     }
-    cout << ans[n - 1];
+    cout << dp.back();
 }
+	
+
