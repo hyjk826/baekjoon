1d761437291d2b9b860908da3920f65eab60ce93
hyjk826 <hyjk826@naver.com>
2022-09-14 22:03:44 +0900

    [Gold II] Title: 1의 개수 세기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9527. 1의 개수 세기/1의 개수 세기.cc
+++ 백준/Gold/9527. 1의 개수 세기/1의 개수 세기.cc
@@ -1,4 +1,4 @@
-// 2022-06-22
+// 2022-09-14
 #include <bits/stdc++.h>
 #define fastio                    \
 	ios_base::sync_with_stdio(0); \
@@ -6,27 +6,32 @@
 #define vi vector<int>
 #define vl vector<long long>
 #define vc vector<char>
+#define vs vector<string>
 #define pi pair<int, int>
+#define pl pair<ll, ll>
 #define vp vector<pi>
 #define ll long long
 #define MAX 2147000000
 #define MOD 1000000007
 using namespace std;
 
-ll f(ll a){
-    ll r{0};
-    for(int i{1}; i <= 60; ++i){
-        ll q = (a + 1) / (1LL << i);
-        ll w = (a + 1) % (1LL << i);
-        r += q * (1LL << (i - 1));
-        r += max(0LL, w - (1LL << (i - 1)));
+ll f(ll n){
+    ll ret{0};
+    n++;
+    for(int i{0}; i < 60; ++i){
+        ll k = (1LL << (i + 1));
+        ll r = n % k;
+        ret += n / k * (k / 2);
+        ret += max(0LL, r - k / 2);
     }
-    return r;
+    return ret;
 }
 
-int main(){
-    fastio;
-	ll a, b;
+int main() {
+	fastio;
+    ll a, b;
     cin >> a >> b;
     cout << f(b) - f(a - 1);
 }
+	
+
