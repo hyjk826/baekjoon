9515932b925a89b2b123fd51142697d0c5ad8766
hyjk826 <hyjk826@naver.com>
2022-09-14 18:27:51 +0900

    [Gold III] Title: 피리 부는 사나이, Time: 40 ms, Memory: 11016 KB -BaekjoonHub

--- 백준/Gold/16724. 피리 부는 사나이/피리 부는 사나이.cc
+++ 백준/Gold/16724. 피리 부는 사나이/피리 부는 사나이.cc
@@ -1,4 +1,4 @@
-// 2022-06-24
+// 2022-09-14
 #include <bits/stdc++.h>
 #define fastio                    \
 	ios_base::sync_with_stdio(0); \
@@ -6,50 +6,63 @@
 #define vi vector<int>
 #define vl vector<long long>
 #define vc vector<char>
+#define vs vector<string>
 #define pi pair<int, int>
+#define pl pair<ll, ll>
 #define vp vector<pi>
 #define ll long long
 #define MAX 2147000000
 #define MOD 1000000007
 using namespace std;
 
-int main(){
-    fastio;
-    int n, m;
-    cin >> n >> m; 
-    vector<vc> board(n, vc(m));
+int n, m;
+vector<vc> board(1000, vc(1000));
+int ans{0};
+vector<vi> ch(1000, vi(1000));
+vector<vi> finished(1000, vi(1000));
+string str = "UDLR";
+int xx[]{-1, 1, 0, 0};
+int yy[]{0, 0, -1, 1};
+
+void dfs(int x, int y){
+    ch[x][y] = 1;
+    int index;
+    for(int i{0}; i < 4; ++i){
+        if(str[i] == board[x][y]){
+            index = i;
+            break;
+        }
+    }
+    int nx = x + xx[index];
+    int ny = y + yy[index];
+    if(ch[nx][ny] == 0){
+       dfs(nx, ny); 
+    }
+    else{
+        if(finished[nx][ny] == 0){
+            ans++;
+        }
+    }
+    finished[x][y] = 1;
+}
+
+
+int main() {
+	fastio;
+    cin >> n >> m;
     for(int i{0}; i < n; ++i){
         for(int j{0}; j < m; ++j){
             cin >> board[i][j];
         }
     }
-    vector<vi> ch(n, vi(m));
-    int ans{0};
     for(int i{0}; i < n; ++i){
         for(int j{0}; j < m; ++j){
             if(ch[i][j] == 0){
-                int a = i;
-                int b = j;
-                set<pi> s;
-                while(1){
-                    if(ch[a][b] == 1){
-                        break;    
-                    }
-                    if(s.count({a, b})){
-                        ans++;
-                        break;
-                    }
-                    s.insert({a, b});
-                    if(board[a][b] == 'D') a++;
-                    else if(board[a][b] == 'U') a--;
-                    else if(board[a][b] == 'L') b--;
-                    else b++;
-                }
-                for(auto& k : s){
-                    ch[k.first][k.second] = 1;
-                }
+                dfs(i, j);
             }
         }
     }
     cout << ans;
 }
+	
+
