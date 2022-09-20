4576cfd77c0cccf3eb99f3fc3c2c241928602783
hyjk826 <hyjk826@naver.com>
2022-09-20 19:08:40 +0900

    [Silver IV] Title: 동전 0, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.cc
+++ 백준/Silver/11047. 동전 0/동전 0.cc
@@ -1,31 +1,32 @@
-// 2022-09-10
-#include <bits/stdc++.h>
-#define fastio                    \
-	ios_base::sync_with_stdio(0); \
-	cin.tie(0);
-#define vi vector<int>
-#define vl vector<long long>
-#define vc vector<char>
-#define vs vector<string>
-#define pi pair<int, int>
-#define vp vector<pi>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <queue>
+#include <algorithm>
 #define ll long long
+#define pi pair<int, int>
+#define veci vector<int>
 #define MAX 2147000000
-#define MOD 1000000007
+#define mod 1000000000
 using namespace std;
 
-int main(){
-    fastio;
-    int n, k;
-    cin >> n >> k;
-    vi vec(n);
-    for(int i{0}; i < n; ++i){
-        cin >> vec[i];
-    }
-    int ans{0};
-    for(int i{n - 1}; i >= 0; --i){
-        ans += k / vec[i];
-        k %= vec[i];
-    }
-    cout << ans;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int n, k;
+	cin >> n >> k;
+	vector<int> vec(n);
+	for (int i{ 0 }; i < n; ++i) {
+		cin >> vec[i];
+	}
+	int cnt{ 0 };
+	int i{ n - 1 };
+	while (k != 0) {
+		if (k >= vec[i]) {
+			cnt += k / vec[i];
+			k %= vec[i];
+		}
+		i--;
+	}
+	cout << cnt;
 }
