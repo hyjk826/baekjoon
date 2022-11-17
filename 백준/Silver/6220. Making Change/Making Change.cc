00ce0026e1204c024995718ca69b9659f0593d20
hyjk826 <hyjk826@naver.com>
2022-11-17 11:39:18 +0900

    [Silver II] Title: Making Change, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/6220. Making Change/Making Change.cc
@@ -0,0 +1,39 @@
+// 2022-11-16
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int c, n;
+vi vec(10);
+
+
+int main() {
+	fastio;
+    cin >> c >> n;
+    for(int i{0}; i < n; ++i){
+        cin >> vec[i];
+    }
+    vi dp(c + 1, MAX);
+    dp[0] = 0;
+    for(int i{0}; i < n; ++i){
+        for(int j{0}; j <= c - vec[i]; ++j){
+            dp[j + vec[i]] = min(dp[j + vec[i]], dp[j] + 1);
+        }
+    }
+    cout << dp[c];
+}
+	
+
