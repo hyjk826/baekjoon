1d60296f3fe080859806b4d468230a8f361f5c1c
hyjk826 <hyjk826@naver.com>
2022-11-21 20:21:09 +0900

    [Silver III] Title: 삼삼한 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/17252. 삼삼한 수/삼삼한 수.cc
@@ -0,0 +1,37 @@
+// 2022-11-21
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main() {
+	fastio;	
+	int n;
+    cin >> n;
+    if(n == 0){
+        cout << "NO";
+        return 0;
+    }
+    while(n){
+        if(n % 3 == 2){
+            cout << "NO";
+            return 0;
+        }
+        n /= 3;
+    }
+    cout << "YES";
+}
+	
+
