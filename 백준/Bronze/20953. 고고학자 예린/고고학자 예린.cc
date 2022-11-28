5ae920ee1cce76c033abf2a6c91872cb8ed05250
hyjk826 <hyjk826@naver.com>
2022-11-28 18:15:24 +0900

    [Bronze II] Title: 고고학자 예린, Time: 28 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20953. 고고학자 예린/고고학자 예린.cc
@@ -0,0 +1,31 @@
+// 2022-11-28
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+
+int main() {
+	fastio;
+	int t;
+    cin >> t;
+    while(t--){
+        int a, b;
+        cin >> a >> b;
+        int k = a + b - 1;
+        cout << k * (k + 1) / 2 * (a + b) << "\n";
+    }
+}
+	
