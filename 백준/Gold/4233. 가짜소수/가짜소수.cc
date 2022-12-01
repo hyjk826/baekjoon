3dcd3ff466d775cb13a1ee2cd559d5af68f2708b
hyjk826 <hyjk826@naver.com>
2022-12-01 16:51:41 +0900

    [Gold IV] Title: 가짜소수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/4233. 가짜소수/가짜소수.cc
@@ -0,0 +1,80 @@
+// 2022-12-01
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+ll modpow(ll a, ll b){
+    ll c = b;
+    ll ans{1};
+    while(b > 0){
+        if(b % 2){
+            ans *= a;
+            ans %= c;
+        }
+        a *= a;
+        b /= 2;
+        a %= c;
+    }
+    return ans;
+}
+
+typedef unsigned long long ull;
+
+ll prime[7] = {2, 7, 61}; // 40억 미만 일 경우 2, 7, 61
+
+ull modpow(ull a, ull b, ull mod){
+    ull ans{1};
+    while(b){
+        if(b & 1){
+            ans = (ans * a) % mod;            
+        }
+        b >>= 1;
+        a = (a * a) % mod;
+    }
+    return ans;
+}
+
+bool miller_rabin(ull n, ull a){
+    if(a % n == 0) return 1;
+    ull k = n - 1;
+    while(k % 2 == 0){
+        if(modpow(a, k, n) == n - 1) return 1;
+        k /= 2;
+    }
+    ull temp = modpow(a, k, n);
+    return (temp == 1 || temp == n - 1);
+}
+
+bool check_prime(ull n){
+    for(int i{0}; i < 3; ++i){
+        if(!miller_rabin(n, prime[i])) return 0;
+    }
+    return 1;
+}
+
+
+
+
+int main() {
+	fastio;
+    ll p, a;
+	while(cin >> p >> a){
+        if(p == 0 && a == 0) break;
+        if(!check_prime(p) && modpow(a, p) == a) cout << "yes\n";
+        else cout << "no\n";
+    }
+}
+	
