75aaaf958d9e32725d164a786eb19ae33f6ffce2
hyjk826 <hyjk826@naver.com>
2022-12-07 16:02:20 +0900

    [Platinum I] Title: 트리의 팔, Time: 468 ms, Memory: 59720 KB -BaekjoonHub

---
+++ 백준/Platinum/26161. 트리의 팔/트리의 팔.cc
@@ -0,0 +1,123 @@
+// 2022-12-07
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+const double PI = acos(-1);
+typedef complex<double> cpx;
+
+void FFT(vector<cpx> &v, bool inv) {
+    int S = v.size();
+
+    for(int i=1, j=0; i<S; i++) {
+        int bit = S/2;
+
+        while(j >= bit) {
+            j -= bit;
+            bit /= 2;
+        }
+        j += bit;
+
+        if(i < j) swap(v[i], v[j]);
+    }
+
+    for(int k=1; k<S; k*=2) {
+        double angle = (inv ? PI/k : -PI/k);
+        cpx w(cos(angle), sin(angle));
+
+        for(int i=0; i<S; i+=k*2) {
+            cpx z(1, 0);
+
+            for(int j=0; j<k; j++) {
+                cpx even = v[i+j];
+                cpx odd = v[i+j+k];
+
+                v[i+j] = even + z*odd;
+                v[i+j+k] = even - z*odd;
+
+                z *= w;
+            }
+        }
+    }
+
+    if(inv)
+        for(int i=0; i<S; i++) v[i] /= S;
+}
+
+vector<int> mul(vector<int> &v, vector<int> &u) {
+    vector<cpx> A(v.begin(), v.end());
+    vector<cpx> B(u.begin(), u.end());
+
+    int S = 2;
+    while(S < (int)v.size() + (int)u.size()) S *= 2;
+
+    A.resize(S); FFT(A, false);
+    B.resize(S); FFT(B, false);
+
+    for(int i=0; i<S; i++) A[i] *= B[i];
+    FFT(A, true);
+
+    vector<int> w(S);
+    for(int i=0; i<S; i++) w[i] = round(A[i].real());
+
+    return w;
+}
+
+int main() {
+	fastio;
+	int n, R;
+    cin >> n >> R;
+    vector<vi> g(n + 1);
+    for(int i{0}; i < n - 1; ++i){
+        int a, b;
+        cin >> a >> b;
+        g[a].push_back(b);
+        g[b].push_back(a);
+    }
+    vi dist(n + 1, -1);
+    queue<int> Q;
+    Q.push(R);
+    dist[R] = 0;
+    while(!Q.empty()){
+        int x{Q.front()};
+        Q.pop();
+        for(auto& i : g[x]){
+            if(dist[i] == -1){
+                dist[i] = dist[x] + 1;
+                Q.push(i);
+            }
+        }
+    }
+    vi A(n);
+    for(int i{1}; i <= n; ++i){
+        if(i == R) continue;
+        if(g[i].size() == 1) A[dist[i]]++;
+    }
+    vi B = A;
+    vi r = mul(A, B);
+    r.erase(r.begin() + A.size() + B.size() - 1, r.end());
+    for(int i{1}; i < (int)r.size(); ++i){
+        r[i] += r[i - 1];
+        r[i] %= MOD;
+    }
+    int q;
+    cin >> q;
+    while(q--){
+        int a, b;
+        cin >> a >> b;
+        cout << (r[b] - r[a - 1] + MOD) % MOD << "\n";
+    }
+}
+
