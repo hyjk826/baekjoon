ad4d4a06e6cc7bf164a0cff07984173a83b73df3
hyjk826 <hyjk826@naver.com>
2022-12-12 20:10:42 +0900

    [Gold II] Title: 선진이의 겨울 왕국, Time: 4 ms, Memory: 2288 KB -BaekjoonHub

---
+++ 백준/Gold/11567. 선진이의 겨울 왕국/선진이의 겨울 왕국.cc
@@ -0,0 +1,64 @@
+// 2022-12-12
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main() {
+	fastio;
+    int n, m;
+    cin >> n >> m;
+    vector<vc> board(n, vc(m));
+    for(int i{0}; i < n; ++i){
+        for(int j{0}; j < m; ++j){
+            cin >> board[i][j];
+        }
+    }
+    int sx, sy, tx, ty;
+    cin >> sx >> sy >> tx >> ty;
+    sx--; sy--; tx--; ty--;
+    queue<pi> Q;
+    Q.push({sx, sy});
+    int xx[]{-1,0,1,0};
+    int yy[]{0,1,0,-1};
+    while(!Q.empty()){
+        int x{Q.front().first};
+        int y{Q.front().second};
+        Q.pop();
+        for(int dir{0}; dir < 4; ++dir){
+            int nx{x + xx[dir]};
+            int ny{y + yy[dir]};
+            if(nx < 0 || nx > n - 1 || ny < 0 || ny > m - 1) continue;
+            if(nx == tx && ny == ty){
+                if(board[nx][ny] == 'X'){
+                    cout << "YES";
+                    return 0;
+                }
+                else{
+                    board[nx][ny] = 'X';
+                    Q.push({nx, ny});
+                }
+            }
+            else{
+                if(board[nx][ny] == '.'){
+                    board[nx][ny] = 'X';
+                    Q.push({nx, ny});
+                }
+            }
+        }
+    }
+    cout << "NO";
+}
+	
