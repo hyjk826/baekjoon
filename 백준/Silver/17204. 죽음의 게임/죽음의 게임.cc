5f76f805ab9f63e7bf2622b248b36d21ac529a43
hyjk826 <hyjk826@naver.com>
2022-12-14 16:47:42 +0900

    [Silver III] Title: 죽음의 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/17204. 죽음의 게임/죽음의 게임.cc
@@ -0,0 +1,37 @@
+// 2022-12-14
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main() {
+	fastio;
+    int n, k;
+    cin >> n >> k;
+    vi vec(n);
+    for(int i{0}; i < n; ++i){
+        cin >> vec[i];
+    }
+    int a = 0;
+    for(int i{0}; i < n; ++i){
+        a = vec[a];
+        if(a == k){
+            cout << i + 1; 
+            return 0;
+        }
+    }
+    cout << -1;
+}
+	
