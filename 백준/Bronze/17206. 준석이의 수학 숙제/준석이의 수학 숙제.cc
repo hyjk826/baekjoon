d58f5414258f50a4557b87fade94356f2c875d8b
hyjk826 <hyjk826@naver.com>
2022-12-14 16:10:41 +0900

    [Bronze I] Title: 준석이의 수학 숙제, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/17206. 준석이의 수학 숙제/준석이의 수학 숙제.cc
@@ -0,0 +1,33 @@
+// 2022-12-14
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main() {
+	fastio;
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        auto f = [&](int k){
+            int a = n / k;
+            return 1LL * k * a * (a + 1) / 2;
+        };
+        cout << f(3) + f(7) - f(21) << "\n";
+    }
+}
+	
