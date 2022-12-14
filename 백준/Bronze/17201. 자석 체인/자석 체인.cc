3efa5113ace11f7873d73a24594308b8766260d4
hyjk826 <hyjk826@naver.com>
2022-12-14 15:37:11 +0900

    [Bronze II] Title: 자석 체인, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/17201. 자석 체인/자석 체인.cc
@@ -0,0 +1,33 @@
+// 2022-12-14
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+
+int main() {
+	fastio;
+    int n;
+    cin >> n;
+    string str;
+    cin >> str;
+    bool ok = true;
+    for(int i{0}; i < 2 * n - 1; ++i){
+        ok &= (str[i] != str[i + 1]);
+    }
+    if(ok) cout << "Yes";
+    else cout << "No";
+}
+	
