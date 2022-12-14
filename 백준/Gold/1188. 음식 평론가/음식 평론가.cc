7a3eee96e17757b33e5b246d545bbb2d671d1333
hyjk826 <hyjk826@naver.com>
2022-12-14 14:17:20 +0900

    [Gold V] Title: 음식 평론가, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/1188. 음식 평론가/음식 평론가.cc
@@ -0,0 +1,30 @@
+// 2022-12-14
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int gcd(int a, int b){
+    if(b == 0) return a;
+    return gcd(b, a % b);
+}
+
+int main() {
+	fastio;
+    int n, m;
+    cin >> n >> m;
+    cout << m - gcd(n, m);
+}
+	
