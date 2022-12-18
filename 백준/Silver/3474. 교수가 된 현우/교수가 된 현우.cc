cb80a04901ff10509ba4990fcdefc18bca7c7ea1
hyjk826 <hyjk826@naver.com>
2022-12-18 21:56:57 +0900

    [Silver III] Title: 교수가 된 현우, Time: 24 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/3474. 교수가 된 현우/교수가 된 현우.cc
@@ -0,0 +1,35 @@
+// 2022-12-18
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main(){
+	fastio;
+	int t;
+    cin >> t;
+    while(t--){
+        ll ans{0};
+        ll n;
+        cin >> n;
+        ll k{5};
+        while(k <= n){
+            ans += n / k;
+            k *= 5;
+        }
+        cout << ans << "\n";
+    }
+}
+	
