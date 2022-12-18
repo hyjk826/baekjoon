1d42f31c1e05647ab847c34c306ddee7b75d7d31
hyjk826 <hyjk826@naver.com>
2022-12-19 03:04:34 +0900

    [Silver I] Title: 타일 위의 대각선, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2168. 타일 위의 대각선/타일 위의 대각선.cc
@@ -0,0 +1,31 @@
+// 2022-12-18
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int gcd(int a, int b){
+    if(b == 0) return a;
+    return gcd(b, a % b);
+}
+
+int main(){
+	fastio;
+	int a, b;
+    cin >> a >> b;
+    int g = gcd(a, b);
+    cout << 1LL * g * (a / g + b / g - 1);
+}
+	
