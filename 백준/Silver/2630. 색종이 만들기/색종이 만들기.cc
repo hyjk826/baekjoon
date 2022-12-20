4e17ab14c1eb197e1b99fa1cf5577869103b77c1
hyjk826 <hyjk826@naver.com>
2022-12-20 16:44:13 +0900

    [Silver II] Title: 색종이 만들기, Time: 0 ms, Memory: 2080 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.cc
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.cc
@@ -1,51 +1,56 @@
-// 2022-09-10
-#include <bits/stdc++.h>
-#define fastio                    \
-	ios_base::sync_with_stdio(0); \
-	cin.tie(0);
-#define vi vector<int>
-#define vl vector<long long>
-#define vc vector<char>
-#define vs vector<string>
-#define pi pair<int, int>
-#define vp vector<pi>
+#include <iostream>
+#include <string>
+#include <queue>
+#include <vector>
+#include <cstring>
+#include <algorithm>
+using namespace std;
 #define ll long long
 #define MAX 2147000000
-#define MOD 1000000007
-using namespace std;
 
-int n;
-int board[128][128];
-int zero{0}, one{0};
+int map[128][128];
+int white{ 0 };
+int blue{ 0 };
 
-void f(int x, int y, int k){
-    int sum{0};
-    for(int i{x}; i < x + k; ++i){
-        for(int j{y}; j < y + k; ++j){
-            sum += board[i][j];
-        }
-    }
-    if(sum == k * k){
-        one++;
-    }
-    else if(sum == 0){
-        zero++;
-    }
-    else{
-        f(x, y, k / 2);
-        f(x, y + k / 2, k / 2);
-        f(x + k / 2, y, k / 2);
-        f(x + k / 2, y + k / 2, k / 2);
-    }
+int check(int s, int x, int y) {
+	for (int i{ x }; i < x + s; ++i) {
+		for (int j{ y }; j < y + s; ++j) {
+			if (map[i][j] != map[x][y]) {
+				return false;
+			}
+		}
+	}
+	return true;
 }
-int main(){
-    fastio;
-    cin >> n;
-    for(int i{0}; i < n; ++i){
-        for(int j{0}; j < n; ++j){
-            cin >> board[i][j];
-        }
-    }
-    f(0, 0, n);
-    cout << zero << "\n" << one;   
+
+void recur(int s, int x, int y) {
+	if (check(s, x, y)) {
+		if (map[x][y] == 0) {
+			white++;
+		}
+		else if (map[x][y] == 1) {
+			blue++;
+		}
+	}
+	else {
+		recur(s / 2, x, y);
+		recur(s / 2, x, y + s / 2);
+		recur(s / 2, x + s / 2, y);
+		recur(s / 2, x + s / 2, y + s / 2);
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int n;
+	cin >> n;
+	for (int i{ 0 }; i < n; ++i) {
+		for (int j{ 0 }; j < n; ++j) {
+			cin >> map[i][j];
+		}
+	}
+	recur(n, 0, 0);
+	cout << white << "\n";
+	cout << blue;
 }
