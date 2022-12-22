8453709e934b7b409473ddc5a0e43ee9a2695194
hyjk826 <hyjk826@naver.com>
2022-12-23 01:11:37 +0900

    [Bronze III] Title: 겨울왕국 티켓 예매, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18247. 겨울왕국 티켓 예매/겨울왕국 티켓 예매.cc
@@ -0,0 +1,28 @@
+// 2022-12-22
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main(){
+	int t;
+    cin >> t;
+    while(t--){
+        int n, m;
+        cin >> n >> m;
+        if(n <= 11 || m <= 3) cout << -1 << "\n";
+        else cout << 11 * m + 4 << "\n";
+    }
+}
