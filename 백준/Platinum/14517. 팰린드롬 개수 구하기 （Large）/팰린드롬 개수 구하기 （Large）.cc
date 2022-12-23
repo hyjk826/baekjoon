8b819e5902140e8c963bcab75f13f8f6b8c52493
hyjk826 <hyjk826@naver.com>
2022-12-23 16:24:02 +0900

    [Platinum V] Title: 팰린드롬 개수 구하기 (Large), Time: 12 ms, Memory: 5984 KB -BaekjoonHub

---
+++ 백준/Platinum/14517. 팰린드롬 개수 구하기 （Large）/팰린드롬 개수 구하기 （Large）.cc
@@ -0,0 +1,35 @@
+// 2022-12-23
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+vector<vi> ch(1001, vi(1001, -1));
+
+string str;
+
+int f(int l, int r){
+    if(l > r) return 0;
+    if(ch[l][r] != -1) return ch[l][r];
+    if(str[l] == str[r]){
+        return ch[l][r] = (f(l + 1, r) + f(l, r - 1) + 1) % 10007;
+    }
+    else return ch[l][r] = (f(l + 1, r) + f(l, r - 1) - f(l + 1, r - 1) + 10 * 10007) % 10007;
+}
+
+int main(){
+    cin >> str;
+    cout << f(0, (int)str.size() - 1);
+}
