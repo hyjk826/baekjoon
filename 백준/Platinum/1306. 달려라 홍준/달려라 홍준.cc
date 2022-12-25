dada6a0a31a52e6ce6e57a6dd7b7985c3318bfe6
hyjk826 <hyjk826@naver.com>
2022-12-26 08:29:24 +0900

    [Platinum V] Title: 달려라 홍준, Time: 544 ms, Memory: 21556 KB -BaekjoonHub

--- 백준/Platinum/1306. 달려라 홍준/달려라 홍준.cc
+++ 백준/Platinum/1306. 달려라 홍준/달려라 홍준.cc
@@ -16,23 +16,40 @@
 #define MOD 1000000007
 using namespace std;
 
+vi seg(4000010);
+vi vec(1000001);
+
+void update(int node, int l, int r, int idx, int value){
+    if(l > idx || r < idx) return;
+    if(l == r){
+        if(value == 1){
+            seg[node] = l;
+        }
+        else seg[node] = 0;
+    }
+    else{
+        int m = (l + r) / 2;
+        update(node * 2, l, m, idx, value);
+        update(node * 2 + 1, m + 1, r, idx, value);
+        if(vec[seg[node * 2]] > vec[seg[node * 2 + 1]]) seg[node] = seg[node * 2];
+        else seg[node] = seg[node * 2 + 1];
+    }
+}
+
 int main(){
 	fastio;
     int n, m;
     cin >> n >> m;
-    vi vec(n);
-    for(int i{0}; i < n; ++i) {
+    for(int i{1}; i <= n; ++i) {
         cin >> vec[i];
     }
-    map<int, int> mp;
-    for(int i{0}; i < 2 * m - 1; ++i){
-        mp[vec[i]]++;
+    for(int i{1}; i <= 2 * m - 1; ++i){
+        update(1, 1, n, i, 1);
     }
-    cout << prev(mp.end())->first << " ";
-    for(int i{0}; i + 2 * m - 1 < n; ++i){
-        mp[vec[i]]--;
-        if(mp[vec[i]] == 0) mp.erase(vec[i]);
-        mp[vec[i + 2 * m - 1]]++;
-        cout << prev(mp.end())->first << " ";
+    cout << vec[seg[1]] << " ";
+    for(int i{1}; i + 2 * m - 1 <= n; ++i){
+        update(1, 1, n, i, -1);
+        update(1, 1, n, i + 2 * m - 1, 1);
+        cout << vec[seg[1]] << " ";
     }
 }
