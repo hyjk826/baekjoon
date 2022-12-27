5ae3dccadd88d1333dff4f577442a810fdf33f42
hyjk826 <hyjk826@naver.com>
2022-12-28 01:16:47 +0900

    [Platinum IV] Title: 책 구매하기 2, Time: 288 ms, Memory: 2648 KB -BaekjoonHub

---
+++ 백준/Platinum/11406. 책 구매하기 2/책 구매하기 2.cc
@@ -0,0 +1,102 @@
+// 2022-12-27
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+#define MX 205
+vector<vi> g(MX);
+int capacity[MX][MX];
+int flow[MX][MX];
+int d[MX][MX];
+int source = 0;
+int sink = MX - 1;
+
+void addEdge(int a, int b, int c, int dist){
+    g[a].push_back(b);
+    g[b].push_back(a);
+    capacity[a][b] += c;
+    d[a][b] += dist;
+    d[b][a] -= dist;
+}
+
+pi MCMF(){
+    int cost{0};
+    int f{0};
+    while(1){
+        vi p(MX, -1);
+        vi dist(MX, MAX);
+        vi ch(MX);
+        dist[source] = 0;
+        ch[source] = 1;
+        queue<int> Q;
+        Q.push(source);
+        while(!Q.empty()){
+            int x{Q.front()};
+            Q.pop();
+            ch[x] = 0;
+            for(auto& i : g[x]){
+                if(capacity[x][i] - flow[x][i] > 0 && dist[i] > dist[x] + d[x][i]){
+                    dist[i] = dist[x] + d[x][i];
+                    p[i] = x;
+                    if(!ch[i]){
+                        Q.push(i);
+                        ch[i] = 1;
+                    }
+                }
+            }
+        }
+        if(p[sink] == -1) break;
+        int ff = MAX;
+        for(int i{sink}; i != source; i = p[i]){
+            ff = min(ff, capacity[p[i]][i] - flow[p[i]][i]);
+        }
+        for(int i{sink}; i != source; i = p[i]){
+            cost += ff * d[p[i]][i];
+            flow[p[i]][i] += ff;
+            flow[i][p[i]] -= ff;
+        }
+        f += ff;
+    }
+    return {f, cost};
+}
+	
+int main(){
+	fastio;
+    int n, m;
+    cin >> n >> m;
+    vi A(n + 1), B(m + 1);
+    for(int i{1}; i <= n; ++i){
+        cin >> A[i];
+    }
+    for(int i{1}; i <= m; ++i){
+        cin >> B[i];
+    }
+    for(int i{1}; i <= m; ++i){
+        addEdge(source, i, B[i], 0);
+    }
+    for(int i{1}; i <= n; ++i){
+        addEdge(100 + i, sink, A[i], 0);
+    }
+    for(int i{1}; i <= m; ++i){
+        for(int j{1}; j <= n; ++j){
+            int a;
+            cin >> a;
+            addEdge(i, 100 + j, a, 0);
+        }
+    }
+    pi ans = MCMF();
+    cout << ans.first;
+}
