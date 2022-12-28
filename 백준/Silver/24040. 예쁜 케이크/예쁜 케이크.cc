13ff1ce9857238a093ed9e002c013dbfe38dc552
hyjk826 <hyjk826@naver.com>
2022-12-28 10:36:02 +0900

    [Silver II] Title: 예쁜 케이크, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/24040. 예쁜 케이크/예쁜 케이크.cc
@@ -0,0 +1,29 @@
+// 2022-12-27
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main(){
+	fastio;
+    int t;
+    cin >> t;
+    while(t--){
+        ll n;
+        cin >> n;
+        if(n % 9 == 0 || n % 3 == 2) cout << "TAK\n";
+        else cout << "NIE\n";
+    }
+}
