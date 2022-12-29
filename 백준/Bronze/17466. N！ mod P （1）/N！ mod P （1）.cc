ca575388565eaf74756cbdc36aeab56bfeab8b74
hyjk826 <hyjk826@naver.com>
2022-12-30 05:12:16 +0900

    [Bronze II] Title: N! mod P (1), Time: 1472 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/17466. N！ mod P （1）/N！ mod P （1）.cc
@@ -0,0 +1,30 @@
+// 2022-12-30
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+
+int main(){
+	fastio;
+    int n, p;
+    cin >> n >> p;
+    ll ans{1};
+    for(int i{1}; i <= n; ++i){
+        ans *= i;
+        ans %= p;
+    }
+    cout << ans;
+}
