718bbf120161a654fd43bb6303c9340fdcb0d418
hyjk826 <hyjk826@naver.com>
2023-01-03 01:29:34 +0900

    [Gold III] Title: 연속 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/2737. 연속 합/연속 합.cc
@@ -0,0 +1,36 @@
+// 2023-01-03
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+
+int main(){
+	fastio;
+    int t;
+    cin >> t;
+    while(t--){
+        ll n;
+        cin >> n;
+        int ans{0};
+        for(ll i{2}; i * (i - 1) / 2 < n; ++i){
+            ll a = n - i * (i - 1) / 2;
+            if(a % i == 0) {
+                ans++;
+            }
+        }
+        cout << ans << "\n";
+    }
+}
