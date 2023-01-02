0dbd697c21fc1e58a13adbb4c49450348831a696
hyjk826 <hyjk826@naver.com>
2023-01-03 02:20:56 +0900

    [Diamond V] Title: Non-Squarefree Numbers, Time: 104 ms, Memory: 8276 KB -BaekjoonHub

---
+++ 백준/Diamond/8464. Non－Squarefree Numbers/Non－Squarefree Numbers.cc
@@ -0,0 +1,51 @@
+// 2023-01-03
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main() {
+	fastio;
+	ll n;
+    cin >> n;
+    const int sz = 8e5;
+    vi A(sz + 1, -1);
+    vi prime(sz + 1);
+    for(int i = 2; i <= sz; i++) {
+        if(prime[i]) continue;
+        A[i] = 1;
+        for(int j = 2 * i; j <= sz; j += i) {
+            prime[j] = 1;
+            if(j % (i * i) == 0) A[j] = 0;
+            else A[j] *= -1;
+        }
+    }
+    ll l{0}, r{(ll)1e12};
+    ll ans{0};
+    while(l <= r){
+        ll m{(l + r) / 2};
+        ll sum{0};
+        for(ll i{2}; i * i <= m; ++i){
+            sum += (m / (i * i)) * A[i];
+        }
+        if(sum >= n){
+            ans = m;
+            r = m - 1;
+        }
+        else l = m + 1;
+    }
+    cout << ans;
+}
+	
