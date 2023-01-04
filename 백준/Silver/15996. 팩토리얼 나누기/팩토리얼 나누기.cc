f6c06f877a6c75218b91c48d9dcb6823132ace16
hyjk826 <hyjk826@naver.com>
2023-01-05 02:26:46 +0900

    [Silver IV] Title: 팩토리얼 나누기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15996. 팩토리얼 나누기/팩토리얼 나누기.cc
@@ -0,0 +1,30 @@
+// 2023-01-04
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main(){
+	fastio;
+    int n, a;
+    cin >> n >> a;
+    ll ans{0};
+    ll b = a;
+    while(b <= n){
+        ans += n / b;
+        b *= a;
+    }
+    cout << ans;
+}
