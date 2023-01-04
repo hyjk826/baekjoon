7697162000f87aa0ad42fbf5fd5a5a02ac2fc690
hyjk826 <hyjk826@naver.com>
2023-01-05 02:19:27 +0900

    [Silver III] Title: 단순한 문제 (Large), Time: 232 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/25487. 단순한 문제 （Large）/단순한 문제 （Large）.cc
@@ -0,0 +1,28 @@
+// 2023-01-04
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main(){
+	fastio;
+    int t;
+    cin >> t;
+    while(t--){
+        int a, b, c;
+        cin >> a >> b >> c;
+        cout << min({a, b, c}) << '\n';
+    }
+}
