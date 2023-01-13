37175b1d1c9ed98bdca602d1fb6fc14239bb7fe0
hyjk826 <hyjk826@naver.com>
2023-01-14 04:04:12 +0900

    [Platinum IV] Title: 평범한 배낭 2, Time: 24 ms, Memory: 2180 KB -BaekjoonHub

---
+++ 백준/Platinum/12920. 평범한 배낭 2/평범한 배낭 2.cc
@@ -0,0 +1,45 @@
+// 2023-01-14
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main(){
+	fastio;
+    int n, m;
+    cin >> n >> m;
+    vi dp(m + 1);
+    dp[0] = 0;
+    vp A;
+    for(int i{0}; i < n; ++i){
+        int a, b, c;
+        cin >> a >> b >> c;
+        int sum{0};
+        for(int j{0}; j < 20; ++j){
+            if(sum + (1 << j) <= c){
+                sum += (1 << j);
+                A.push_back({a * (1 << j), b * (1 << j)});
+            }
+            else break;
+        }
+        A.push_back({a * (c - sum), b * (c - sum)});
+    }
+    for(int i{0}; i < (int)A.size(); ++i){
+        for(int j{m}; j >= A[i].first; --j){
+            dp[j] = max(dp[j], dp[j - A[i].first] + A[i].second);
+        }
+    }
+    cout << dp[m];
+}
