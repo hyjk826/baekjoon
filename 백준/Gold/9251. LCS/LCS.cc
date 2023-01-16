bc0509971a07240f826176bcfeb74b361baae368
hyjk826 <hyjk826@naver.com>
2023-01-16 11:50:09 +0900

    [Gold V] Title: LCS, Time: 4 ms, Memory: 5940 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.cc
+++ 백준/Gold/9251. LCS/LCS.cc
@@ -1,35 +1,29 @@
-// 2022-09-11
-#include <bits/stdc++.h>
-#define fastio                    \
-	ios_base::sync_with_stdio(0); \
-	cin.tie(0);
-#define vi vector<int>
-#define vl vector<long long>
-#define vc vector<char>
-#define vs vector<string>
-#define pi pair<int, int>
-#define vp vector<pi>
-#define ll long long
-#define MAX 2147000000
-#define MOD 1000000007
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <algorithm>
+#include <string>
 using namespace std;
 
-int main(){
-    fastio;
-    string s, t;
-    cin >> s >> t;
-    int n = (int)s.size();
-    int m = (int)t.size();
-    vector<vi> board(n + 1, vi(m + 1));
-    for(int i{1}; i <= n; ++i){
-        for(int j{1}; j <= m; ++j){
-            if(s[i - 1] == t[j - 1]){
-                board[i][j] = board[i - 1][j - 1] + 1;
-            }
-            else{
-                board[i][j] = max(board[i - 1][j], board[i][j - 1]);
-            }
-        }
-    }
-    cout << board[n][m];
+int dp[1001][1001]{};
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	string str1;
+	string str2;
+	cin >> str1;
+	cin >> str2;
+	int s1 = (int)str1.size();
+	int s2 = (int)str2.size();
+	for (int i{ 1 }; i <= s1; ++i) {
+		for (int j{ 1 }; j <= s2; ++j) {
+			if (str1[i-1] == str2[j-1]) {
+				dp[i][j] = dp[i - 1][j - 1] + 1;
+			}
+			else {
+				dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+			}
+		}
+	}
+	cout << dp[s1][s2];
 }
