8ba3376b42b64aa35ad86fc481216fcee77010ea
hyjk826 <hyjk826@naver.com>
2023-01-18 01:04:35 +0900

    [Gold III] Title: 줄 세우기, Time: 108 ms, Memory: 9836 KB -BaekjoonHub

---
+++ 백준/Gold/7570. 줄 세우기/줄 세우기.cc
@@ -0,0 +1,34 @@
+// 2023-01-17
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+
+vi dp(1000001);
+
+int main(){
+	fastio;
+    int n;
+    cin >> n;
+    vi vec(n);
+    for(int i{0}; i < n; ++i){
+        cin >> vec[i];
+    }
+    for(int i{0}; i < n; ++i){
+        dp[vec[i]] = dp[vec[i] - 1] + 1;
+    }
+    cout << n - *max_element(dp.begin(), dp.end());
+}
