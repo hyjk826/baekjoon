f09f1a86d41d6c3c6a74ecc3e9c804ddc9fe4e59
hyjk826 <hyjk826@naver.com>
2023-01-18 21:21:00 +0900

    [Gold IV] Title: 문자열 폭발, Time: 12 ms, Memory: 6056 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.cc
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.cc
@@ -1,43 +1,45 @@
-// 2022-09-13
-#include <bits/stdc++.h>
-#define fastio                    \
-	ios_base::sync_with_stdio(0); \
-	cin.tie(0);
+#include <iostream>
+#include <queue>
+#include <string>
+#include <stack>
+#include <map>
+#include <algorithm>
+#include <set>
+#include <cmath>
+#include <unordered_set>
+#include <map>
+#include <cstring>
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0);
 #define vi vector<int>
-#define vl vector<long long>
-#define vc vector<char>
-#define vs vector<string>
 #define pi pair<int, int>
-#define pl pair<ll, ll>
-#define vp vector<pi>
 #define ll long long
 #define MAX 2147000000
-#define MOD 1000000007
+#define MOD 998244353LL
 using namespace std;
 
-int main() {
+int main(){
     fastio;
-    string s, t;
+    string s, t, ans;
     cin >> s >> t;
-    string str;
     for (int i{ 0 }; i < (int)s.size(); ++i) {
-        str += s[i];
-        bool flag = false;;
-        if (str.size() >= t.size() && str.back() == t.back()) {
-            flag = true;
-            for (int j{ 0 }; j < (int)t.size(); ++j) {
-                if (t[j] != str[(int)str.size() - (int)t.size() + j]) {
-                    flag = false;
-                    break;
+        ans += s[i];
+        if (ans.back() == t.back()) {
+            if (ans.size() >= t.size()) {
+                bool flag = true;
+                for (int i{ (int)t.size() - 2 }, k{ 1 }; i >= 0; --i, ++k) {
+                    if (ans[(int)ans.size() - 1 - k] != t[i]) {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag) {
+                    for (int i{ 0 }; i < (int)t.size(); ++i) {
+                        ans.pop_back();
+                    }
                 }
             }
         }
-        if (flag) {
-            for (int j{ 0 }; j < (int)t.size(); ++j) str.pop_back();
-        }
     }
-    if (str.size() == 0) cout << "FRULA";
-    else cout << str;
+    if (ans.size() == 0) cout << "FRULA";
+    else cout << ans;
 }
-
-
