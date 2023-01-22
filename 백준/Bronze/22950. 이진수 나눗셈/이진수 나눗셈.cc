48a05530984f0ddbb9b9213500f08c778921f9fa
hyjk826 <hyjk826@naver.com>
2023-01-22 20:40:22 +0900

    [Bronze I] Title: 이진수 나눗셈, Time: 8 ms, Memory: 3680 KB -BaekjoonHub

---
+++ 백준/Bronze/22950. 이진수 나눗셈/이진수 나눗셈.cc
@@ -0,0 +1,34 @@
+// 2023-01-22
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main(){
+	fastio;
+    int n;
+    cin >> n;
+    string str;
+    cin >> str;
+    reverse(str.begin(), str.end());
+    bool ok = true;
+    int m;
+    cin >> m;
+    for(int i{0}; i < min(n, m); ++i){
+        ok &= (str[i] == '0');
+    }
+    if(ok) cout << "YES";
+    else cout << "NO";
+}
