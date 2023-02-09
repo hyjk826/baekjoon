155fdc8924c04b44ce1d32bc21e32a62d4832f40
hyjk826 <hyjk826@naver.com>
2023-02-09 22:12:50 +0900

    [Bronze IV] Title: Equality, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/13985. Equality/Equality.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main(){
+	fastio;
+    string str;
+    getline(cin, str);
+    if(str[0] - '0' + str[4] - '0' == str[8] - '0') cout << "YES";
+    else cout << "NO";
+}
