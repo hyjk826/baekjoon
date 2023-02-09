52b62e855fd88be9ef0378d79d693810a7db6650
hyjk826 <hyjk826@naver.com>
2023-02-09 22:01:47 +0900

    [Gold V] Title: 잠수함식별, Time: 0 ms, Memory: 2176 KB -BaekjoonHub

---
+++ 백준/Gold/2671. 잠수함식별/잠수함식별.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main(){
+	fastio;
+    string str;
+    cin >> str;
+    regex reg("(100+1+|01)+");
+    if(regex_match(str, reg)) cout << "SUBMARINE";
+    else cout << "NOISE";
+}
