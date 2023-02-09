9e9fa59f71812e5dec17e5ce6e2443a5f7321175
hyjk826 <hyjk826@naver.com>
2023-02-09 22:15:24 +0900

    [Bronze III] Title: 한다 안한다, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/5789. 한다 안한다/한다 안한다.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main(){
+	fastio;
+    int t;
+    cin >> t;
+    while(t--){
+        bool flag = true;
+        string str;
+        cin >> str;
+        for(int i{0}; i < (int)str.size() / 2; ++i){
+            if(str[i]== str[(int)str.size() - 1 - i]) flag = true;
+            else flag = false;
+        }
+        if(flag) cout << "Do-it\n";
+        else cout << "Do-it-Not\n";
+    }
+}
