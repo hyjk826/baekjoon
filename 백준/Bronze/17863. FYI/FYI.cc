adc4111c1c54d16b83c95ede571c33a5bffe0037
hyjk826 <hyjk826@naver.com>
2023-02-09 22:54:32 +0900

    [Bronze IV] Title: FYI, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/17863. FYI/FYI.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main(){
+	fastio;
+    string str;
+    cin >> str;
+    if(str.substr(0, 3) == "555") cout << "YES";
+    else cout << "NO";
+}
