c928bc72f124f2376395b962dc403b1c74b3ab8d
hyjk826 <hyjk826@naver.com>
2023-02-10 15:49:18 +0900

    [Bronze I] Title: 데칼코마니, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23841. 데칼코마니/데칼코마니.cc
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main(){
+	fastio;
+    int n, m;
+    cin >> n >> m;
+    vector<vc> board(n, vc(m));
+    for(int i{0}; i < n; ++i){
+        for(int j{0}; j < m; ++j){
+            cin >> board[i][j];
+        }
+    }
+    for(int i{0}; i < n; ++i){
+        for(int j{0}; j < m / 2; ++j){
+            if(board[i][j] != '.') board[i][m - 1 - j] = board[i][j];
+            if(board[i][m - 1 - j] != '.') board[i][j] = board[i][m - 1 - j];
+        }
+    }
+    for(int i{0}; i < n; ++i){
+        for(int j{0}; j < m; ++j){
+            cout << board[i][j];
+        }
+        cout << "\n";
+    }
+}
