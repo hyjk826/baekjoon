9b2ca4d90a8c80b085c2dc9efc990736b8351054
hyjk826 <hyjk826@naver.com>
2023-02-15 02:16:49 +0900

    [Platinum III] Title: Suffix Array, Time: 224 ms, Memory: 14396 KB -BaekjoonHub

--- 백준/Platinum/9248. Suffix Array/Suffix Array.cc
+++ 백준/Platinum/9248. Suffix Array/Suffix Array.cc
@@ -1,4 +1,3 @@
-// 2022-12-01
 #include <bits/stdc++.h>
 #define fastio                    \
 	ios_base::sync_with_stdio(0); \
@@ -16,72 +15,37 @@
 #define MOD 1000000007
 using namespace std;
 
-vi SuffixArray(string &s) {
-    int n=s.size();
-    int lim = max(257,n+1);
-    vi sfx(n);
-    vi g(n + 1);
-    vi ng(n + 1);
-    vi orderToIdx(n + 1);
-    vi cnt;
-    for(int i=0; i<n; i++) sfx[i]=i, g[i]=s[i];
-    for(int t=1; t<n; t<<=1) {
-        cnt.clear(); cnt.resize(lim,0);
-        for(int i=0; i<n; i++) cnt[g[min(i+t,n)]]++;
-        for(int i=1; i<lim; i++) cnt[i]+=cnt[i-1];
-        for(int idx=n-1; idx>=0; idx--) orderToIdx[--cnt[g[min(idx+t,n)]]]=idx;
- 
-        cnt.clear(); cnt.resize(lim,0);
-        for(int i=0; i<n; i++) cnt[g[i]]++;
-        for(int i=1; i<lim; i++) cnt[i]+=cnt[i-1];
-        for(int i=n-1; i>=0; i--) sfx[--cnt[g[orderToIdx[i]]]]=orderToIdx[i];
- 
-        auto cmp = [&](int i, int j) {
-            if(g[i]==g[j]) return g[i+t]<g[j+t];
-            else return g[i]<g[j];
-        };
-        ng[sfx[0]]=1;
-        for(int i=1; i<n; i++) {
-            if(cmp(sfx[i-1],sfx[i])) ng[sfx[i]]=ng[sfx[i-1]]+1;
-            else ng[sfx[i]]=ng[sfx[i-1]];
+struct SuffixArray {
+    vi sa , lcp ;
+    SuffixArray ( string &s , int lim = 256 ) {
+        int n = (int)s.size() + 1, k = 0 , a , b ;
+        vi x(s.begin(), s.end() + 1) , y(n), ws(max(n,lim)),rank(n);
+        sa = lcp = y, iota(sa.begin(), sa.end(),0) ;
+        for ( int j = 0 , p = 0 ; p < n ; j = max ( 1 , j * 2 ) , lim = p ) {
+            p = j , iota(y.begin(), y.end(),n-j);
+            for(int i{0}; i < n; ++i) if (sa[i] >= j) y[p++] = sa[i] - j;
+            fill(ws.begin(), ws.end(),0);
+            for(int i{0}; i < n; ++i) ws[x[i]]++;
+            for(int i{1}; i < lim; ++i) ws[i] += ws[i-1];
+            for ( int i=n; i--;) sa[--ws[x[y[i]]]] = y[i];
+            swap(x,y), p=1, x[sa[0]]=0;
+            for(int i{1}; i < n; ++i) a = sa[ i - 1 ], b = sa[i], x[b] =
+                (y[a] == y[b] && y[a+j] == y[b+j]) ? p - 1 : p ++ ;
         }
-        swap(g, ng);
+        for(int i{1}; i < n; ++i) rank[sa[i]] = i;
+        for (int i = 0, j; i < n - 1; lcp[rank[i++]] = k)
+            for (k && k--, j = sa[rank[i] - 1];
+                 s[i+k] == s[j+k];k++);
     }
-    return sfx;
-}
+};
 
-vi LcpArray(string& str){
-    vi sfx = SuffixArray(str);
-    int n = (int)str.size();
-    vi rank(n);
-    vi lcp(n);
-    for(int i{0}; i < n; ++i){
-        rank[sfx[i]] = i;
-    }   
-    int len = 0;
-    for(int i{0}; i < n; ++i){
-        int k = rank[i];
-        if(k){
-            int j = sfx[k - 1];
-            while(str[j + len] == str[i + len]) len++;
-            lcp[k] = len;
-            if(len) len--;
-        }
-    }
-    return lcp;
-}
-
-int main() {
+int main(){
 	fastio;
-	string str;
+    string str;
     cin >> str;
-    vi sfx = SuffixArray(str);
-    vi lcp = LcpArray(str);
-    for(auto& i : sfx) cout << i + 1 << " ";
+    SuffixArray sf(str);
+    for(int i{1}; i < (int)sf.sa.size(); ++i) cout << sf.sa[i] + 1 << " ";
     cout << "\n";
-    cout << "x" << " ";
-    for(int i{1}; i < (int)lcp.size(); ++i){
-        cout << lcp[i] << " ";
-    }
+    cout << "x ";
+    for(int i{2}; i < (int)sf.lcp.size(); ++i) cout << sf.lcp[i] << " ";
 }
-	
