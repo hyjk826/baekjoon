60c7278b209acd93fe3239cbf4d7b7e63f371f07
hyjk826 <hyjk826@naver.com>
2023-02-15 22:40:08 +0900

    [Platinum III] Title: 최장 공통 부분 문자열, Time: 68 ms, Memory: 7332 KB -BaekjoonHub

---
+++ 백준/Platinum/9249. 최장 공통 부분 문자열/최장 공통 부분 문자열.cc
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+struct SuffixArray {
+    vi sa , lcp ;
+    SuffixArray ( string &s , int lim = 256 ) {
+        int n = (int)s.size() + 1, k = 0 ,a , b ;
+        vi x(s.begin(), s.end() + 1) , y(n), ws(max(n,lim)),rank(n);
+        sa = lcp = y, iota(sa.begin(), sa.end(),0) ;
+        for ( int j = 0 , p = 0 ; p < n ; j = max ( 1 , j * 2 ) , lim = p ) {
+            p = j , iota(y.begin(), y.end(),n-j);
+            for(int i{0}; i < n; ++i) if (sa[i] >= j) y[p++] = sa[i] - j;
+            fill(ws.begin(), ws.end(),0);
+            for(int i{0}; i < n; ++i) ws[x[i]]++;
+            for(int i{1}; i < lim; ++i) ws[i] += ws[i-1];
+            for ( int i=n; i--;) sa[--ws[x[y[i]]]] = y[i];
+            swap(x,y), p=1, x[sa[0]]=0;
+            for(int i{1}; i < n; ++i) a = sa[ i - 1 ], b = sa[i], x[b] =
+                (y[a] == y[b] && y[a+j] == y[b+j]) ? p - 1 : p ++ ;
+        }
+        for(int i{1}; i < n; ++i) rank[sa[i]] = i;
+        for (int i = 0, j; i < n - 1; lcp[rank[i++]] = k)
+            for (k && k--, j = sa[rank[i] - 1];
+                 s[i+k] == s[j+k];k++);
+    }
+};
+
+
+int main(){
+	fastio;
+    string s, t;
+    cin >> s >> t;
+    int sz = (int)s.size();
+    s += '#';
+    s += t;
+    SuffixArray sfx(s);
+    int mx{0};
+    int idx{-1};
+    for(int i{2}; i < sfx.lcp.size(); ++i){
+        int a = sfx.sa[i - 1];
+        int b = sfx.sa[i];
+        if(min(a, b) <= sz - 1 && max(a, b) >= sz + 1 && min(sz, sfx.lcp[i]) > mx){
+            mx = min(sz, sfx.lcp[i]);
+            idx = a;
+        }
+    }
+    cout << mx << "\n";
+    cout << s.substr(idx, mx);
+}
