6454defa298e03899fee73a9fa9798d5a41f5799
hyjk826 <hyjk826@naver.com>
2023-02-17 02:04:22 +0900

    [Gold IV] Title: 겹치는 선분, Time: 464 ms, Memory: 26728 KB -BaekjoonHub

---
+++ 백준/Gold/1689. 겹치는 선분/겹치는 선분.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main(){
+	fastio;
+    int n;
+    cin >> n;
+    vp A;
+    for(int i{0}; i < n; ++i){
+        int a, b;
+        cin >> a >> b;
+        A.push_back({a, 1});
+        A.push_back({b, -1});
+    }
+    sort(A.begin(), A.end());
+    int ans{1};
+    for(int i{1}; i < (int)A.size(); ++i){
+        A[i].second += A[i - 1].second;
+        ans = max(ans, A[i].second);
+    }
+    cout << ans;
+}
