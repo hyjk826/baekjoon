82a9023df3f275ac8400ea5de75dfe5b4dcdc82a
hyjk826 <hyjk826@naver.com>
2023-02-18 06:14:19 +0900

    [Platinum IV] Title: 노트북의 주인을 찾아서, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Platinum/1298. 노트북의 주인을 찾아서/노트북의 주인을 찾아서.cc
@@ -0,0 +1,53 @@
+// 2022-01-12
+#include <bits/stdc++.h>
+#define fastio                    \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);
+#define vi vector<int>
+#define pi pair<int, int>
+#define ll long long
+#define MAX 2147000000
+#define MOD 998244353LL
+using namespace std;
+
+vector<int> graph[101];
+vector<int> ch(101);
+vector<int> match(101);
+
+bool bipartiteMatching(int x)
+{
+    for (int i{0}; i < (int)graph[x].size(); ++i)
+    {
+        int nx{graph[x][i]};
+        if (ch[nx] == 0)
+        {
+            ch[nx] = 1;
+            if (match[nx] == 0 || bipartiteMatching(match[nx]))
+            {
+                match[nx] = x;
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    for (int i{0}; i < m; ++i)
+    {
+        int a, b;
+        cin >> a >> b;
+        graph[a].push_back(b);
+    }
+    int ans{0};
+    for (int i{1}; i <= n; ++i)
+    {
+        fill(ch.begin(), ch.end(), 0);
+        if (bipartiteMatching(i))
+            ans++;
+    }
+    cout << ans;
+}
