2b80fa3fa27ee495125cdb7383ea5a349961faff
hyjk826 <hyjk826@naver.com>
2023-02-25 15:33:55 +0900

    [Bronze II] Title: 9진수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14491. 9진수/9진수.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main(){
+	fastio;
+    int a;
+    cin >> a;
+    vi vec;
+    while(a){
+        vec.push_back(a % 9);
+        a /= 9;
+    }
+    reverse(vec.begin(), vec.end());
+    for(auto& i : vec) cout << i;
+}
