d06f2b77db43b29a3b6f2528c14cbdff9182eca3
hyjk826 <hyjk826@naver.com>
2023-02-25 15:50:21 +0900

    [Silver V] Title: 과일노리, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/14493. 과일노리/과일노리.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main(){
+	fastio;
+    int n;
+    cin >> n;
+    vp vec(n);
+    for(int i{0}; i < n; ++i){
+        cin >> vec[i].first >> vec[i].second;
+    }
+    int ans{0};
+    for(int i{0}; i < n; ++i){
+        int m = ans % (vec[i].first + vec[i].second);
+        if(m < vec[i].second) ans += vec[i].second - m;
+        ans++;
+    }
+    cout << ans;
+}
