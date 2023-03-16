d80e3109cfb89a2801cf5190e4a16be33610add9
hyjk826 <hyjk826@naver.com>
2023-03-16 17:53:05 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main(){
+	fastio;
+    int n, m;
+    cin >> n >> m;
+    vi vec(n);
+    iota(vec.begin(), vec.end(), 0);
+    for(int i{0}; i < m; ++i){
+        int a, b;
+        cin >> a >> b;
+        a--; b--;
+        reverse(vec.begin() + a, vec.begin() + b + 1);
+    }
+    for(auto& i : vec) cout << i + 1 << " ";
+}
