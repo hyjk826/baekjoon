bf1faf3fe1a0e6a56ed2981cc6a50d3303e137eb
hyjk826 <hyjk826@naver.com>
2023-03-19 13:16:05 +0900

    [Gold IV] Title: 주난의 난(難), Time: 4 ms, Memory: 2552 KB -BaekjoonHub

--- 백준/Gold/14497. 주난의 난（難）/주난의 난（難）.cc
+++ 백준/Gold/14497. 주난의 난（難）/주난의 난（難）.cc
@@ -17,50 +17,47 @@ using namespace std;
 
 int main(){
 	fastio;
-    int n, m;
-    cin >> n >> m;
-    int x1, y1, x2, y2;
-    cin >> x1 >> y1 >> x2 >> y2;
-    x1--; y1--; x2--; y2--;
+    int n, m, sx, sy, tx, ty;
+    cin >> n >> m >> sx >> sy >> tx >> ty;
+    sx--; sy--; tx--; ty--;
     vector<vc> board(n, vc(m));
     for(int i{0}; i < n; ++i){
         for(int j{0}; j < m; ++j){
             cin >> board[i][j];
+            if(board[i][j] == '*') board[i][j] = '0';
+            if(board[i][j] == '#') board[i][j] = '0';
         }
     }
-    int ans{0};
-    while(1){
-        vector<vi> ch(n, vi(m));
-        queue<pi> Q;
-        Q.push({x1, y1});
-        bool flag = false;
-        int xx[]{-1,0,1,0};
-        int yy[]{0,1,0,-1};
-        while(!Q.empty()){
-            int x{Q.front().first};
-            int y{Q.front().second};
-            Q.pop();
-            for(int dir{0}; dir < 4; ++dir){
-                int nx{x + xx[dir]};
-                int ny{y + yy[dir]};
-                if(nx < 0 || nx > n -1 || ny < 0 || ny > m - 1) continue;
-                if(ch[nx][ny] == 0){
-                    ch[nx][ny] = 1;
-                    if(board[nx][ny] == '1'){
-                        board[nx][ny] = '0';
-                        continue;
-                    }
-                    else if(board[nx][ny] == '#'){
-                        flag = true;;
-                        break;
-                    }
-                    Q.push({nx,ny});
+    vector<vi> ch(n, vi(m, -1));
+    deque<pi> dQ;
+    dQ.push_back({sx, sy});
+    ch[sx][sy] = 1;
+    int xx[]{-1,0,1,0};
+    int yy[]{0,1,0,-1};
+    while(!dQ.empty()){
+        int x{dQ.front().first};
+        int y{dQ.front().second};
+        dQ.pop_front();
+        if(x == tx && y == ty){
+            cout << ch[x][y]; return 0;
+        }
+        for(int dir{0}; dir < 4; ++dir){
+            int nx{x + xx[dir]};
+            int ny{y + yy[dir]};
+            if(nx < 0 || nx > n - 1 || ny < 0 || ny > m - 1) continue;
+            if(board[nx][ny] == '0'){
+                if(ch[nx][ny] == -1){
+                    ch[nx][ny] = ch[x][y];
+                    dQ.push_front({nx, ny});
                 }
             }
-            if(flag) break;
+            else{
+                if(ch[nx][ny] == -1){
+                    ch[nx][ny] = ch[x][y] + 1;
+                    dQ.push_back({nx, ny});
+                }
+            }
         }
-        ans++;
-        if(flag) break;
     }
-    cout << ans;
+    
 }
