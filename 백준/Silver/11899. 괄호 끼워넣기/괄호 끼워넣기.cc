f4899fe92e9d6e42d43b1df47729ad786647b531
hyjk826 <hyjk826@naver.com>
2023-03-21 22:43:29 +0900

    [Silver III] Title: 괄호 끼워넣기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11899. 괄호 끼워넣기/괄호 끼워넣기.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main(){
+	fastio;
+    string str;
+    cin >> str;
+    int sum{0};
+    int mn{MAX};
+    for(int i{0}; i < (int)str.size(); ++i){
+        if(str[i] == '(') sum++;
+        else sum--;
+        mn = min(mn, sum);
+    }
+    if(mn < 0) {
+        cout << abs(mn) + sum + abs(mn);
+    }
+    else{
+        cout << sum;
+    }
+}
