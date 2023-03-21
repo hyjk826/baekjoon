8adc31e89f706924ca087b1eb1fc5eee738dc643
hyjk826 <hyjk826@naver.com>
2023-03-21 22:23:56 +0900

    [Bronze II] Title: 박사 과정, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/5026. 박사 과정/박사 과정.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main(){
+	fastio;
+    int t;
+    cin >> t;
+    while(t--){
+        string str;
+        cin >> str;
+        if(str == "P=NP") cout << "skipped\n";
+        else{
+            int idx = str.find('+');
+            int a = stoi(str.substr(0, idx));
+            int b = stoi(str.substr(idx + 1));
+            cout << a + b << "\n";
+        }
+    }
+}
