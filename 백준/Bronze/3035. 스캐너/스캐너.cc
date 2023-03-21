f19e86a158b2aedfe1b690ab0457c39c187fbe0e
hyjk826 <hyjk826@naver.com>
2023-03-21 16:40:10 +0900

    [Bronze I] Title: 스캐너, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3035. 스캐너/스캐너.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main(){
+	fastio;
+    int n, m, a, b;
+    cin >> n >> m >> a >> b;
+    vector<vc> board(n, vc(m));
+    for(int i{0}; i < n; ++i){
+        for(int j{0}; j < m; ++j){
+            cin >> board[i][j];
+        }
+    }
+    for(int i{0}; i < n * a; ++i){
+        for(int j{0}; j < m * b; ++j){
+            cout << board[i / a][j / b];
+        }
+        cout << "\n";
+    }
+}
