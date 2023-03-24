c20cf36ad00b2ae8cb70f8b1aefa229615804892
hyjk826 <hyjk826@naver.com>
2023-03-24 09:23:01 +0900

    [Bronze II] Title: 문자가 몇갤까, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Bronze/7600. 문자가 몇갤까/문자가 몇갤까.cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main(){
+	fastio;
+    string str;
+    while(getline(cin, str)){
+        if(str == "#") break;
+        set<int> st;
+        for(auto& c : str){
+            if(c >= 'a' && c <= 'z') st.insert(c - 'a');
+            if(c >= 'A' && c <= 'Z') st.insert(c - 'A');
+        }
+        cout << st.size() << "\n";
+    }
+}
