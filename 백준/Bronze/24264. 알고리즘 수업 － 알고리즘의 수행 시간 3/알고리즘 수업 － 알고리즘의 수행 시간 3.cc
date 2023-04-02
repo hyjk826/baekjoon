26deaca695f0c1e5c963a225f05dc0893a3d2964
hyjk826 <hyjk826@naver.com>
2023-04-02 17:02:50 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main(){
+	fastio;
+    ll a;
+    cin >> a;
+    cout << a * a << "\n" << 2;
+}
