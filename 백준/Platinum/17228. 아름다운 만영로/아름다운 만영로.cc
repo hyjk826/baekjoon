53d47ff0b73824bac47b2a81a0c6ef36d426e55c
hyjk826 <hyjk826@naver.com>
2023-04-04 10:45:30 +0900

    [Platinum II] Title: 아름다운 만영로, Time: 316 ms, Memory: 78220 KB -BaekjoonHub

--- 백준/Platinum/17228. 아름다운 만영로/아름다운 만영로.cc
+++ 백준/Platinum/17228. 아름다운 만영로/아름다운 만영로.cc
@@ -29,50 +29,51 @@ ll oneHashing(string& str){
 ll k;
 
 const int sz = 5e5 + 10;
+vector<vector<pair<int, char>> > g(sz);
 ll pw1[sz];
 ll pw2[sz];
+vl A, B;
+int ans{0};
+ll a = 1; 
+ll b = 1;
+string str;
+
+void dfs(int cur){
+    if(A.size() > (int)str.size()){
+        ll x = A.back() - A[(int)A.size() - 1 - (int)str.size()] * a;
+        x %= mod1;
+        if(x < 0) x += mod1;
+        ll y = B.back() - B[(int)B.size() - 1 - (int)str.size()] * b;
+        y %= mod2;
+        if(y < 0) y += mod2;
+        if((x << 32 | y) == k) ans++;
+    }
+    for(auto& i : g[cur]){
+        A.push_back((A.back() * p1 + i.second) % mod1);
+        B.push_back((B.back() * p2 + i.second) % mod2);
+        dfs(i.first);
+    }
+    A.pop_back();
+    B.pop_back();
+}
 
 int main(){
 	fastio;
     int n;
-    cin >> n;
-    vector<vector<pair<int, char>> > g(n + 1);
+    cin >> n;    
     for(int i{0}; i < n - 1; ++i){
         int a, b; char c;
         cin >> a >> b >> c;
         g[a].push_back({b, c});
     }
-    string str;
     cin >> str;
     k = oneHashing(str);
-    ll a, b;
-    a = b = 1;
     for(int i{1}; i <= (int)str.size(); ++i){
         a = a * p1 % mod1;
         b = b * p2 % mod2;
-    }
-    vl A, B;
+    }    
     A.push_back(0);
     B.push_back(0);
-    int ans{0};
-    function<void(int)> dfs = [&](int cur){
-        if(A.size() > (int)str.size()){
-            ll x = A.back() - A[(int)A.size() - 1 - (int)str.size()] * a;
-            x %= mod1;
-            if(x < 0) x += mod1;
-            ll y = B.back() - B[(int)B.size() - 1 - (int)str.size()] * b;
-            y %= mod2;
-            if(y < 0) y += mod2;
-            if((x << 32 | y) == k) ans++;
-        }
-        for(auto& i : g[cur]){
-            A.push_back((A.back() * p1 + i.second) % mod1);
-            B.push_back((B.back() * p2 + i.second) % mod2);
-            dfs(i.first);
-        }
-        A.pop_back();
-        B.pop_back();
-    };
     dfs(1);
     cout << ans;
 }
