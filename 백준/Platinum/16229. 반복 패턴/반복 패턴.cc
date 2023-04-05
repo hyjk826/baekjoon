55c68a808660ca0df21c525848435bd57ea2837f
hyjk826 <hyjk826@naver.com>
2023-04-06 08:55:18 +0900

    [Platinum IV] Title: 반복 패턴, Time: 0 ms, Memory: 2784 KB -BaekjoonHub

---
+++ 백준/Platinum/16229. 반복 패턴/반복 패턴.cc
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+vi Z_algorithm(string str){
+    int l{0}, r{0};
+    vi z((int)str.size());
+    for(int i{1}; i < (int)str.size(); ++i){
+        if(i > r){
+            l = r = i;
+            while(r < (int)str.size() && str[r - l] == str[r]) r++;
+            z[i] = r - l; r--;
+        }
+        else{
+            int k = i - l;
+            if(z[k] < r - i + 1) z[i] = z[k];
+            else{
+                l = i;
+                while(r < (int)str.size() && str[r - l] == str[r]) r++;
+                z[i] = r - l; r--;
+            }
+        }
+    }
+    return z;
+}
+
+int main(){
+	fastio;
+    int n, k;
+    cin >> n >> k;
+    if(k >= n){
+        cout << n; return 0;
+    }
+    string str;
+    cin >> str;
+    auto Z = Z_algorithm(str);
+    for(int i{n - 1}; i > 0; --i){
+        if(i + Z[i] != n) continue;
+        if(Z[i] % i == 0 || i - (Z[i] % i) <= k){
+            cout << i; return 0;
+        }
+    }
+    cout << 0;
+}
