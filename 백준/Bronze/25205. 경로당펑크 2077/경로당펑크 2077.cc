7e69a40f62d52b338845455c6a8c57b5ab85a74e
hyjk826 <hyjk826@naver.com>
2023-04-10 01:54:33 +0900

    [Bronze II] Title: 경로당펑크 2077, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/25205. 경로당펑크 2077/경로당펑크 2077.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main(){
+	fastio;
+    int n;
+    cin >> n;
+    string str;
+    cin >> str;
+    string s = "qwertasdfgzxcv";
+    if(s.find(str.back()) != string::npos) cout << 1;
+    else cout << 0;
+}
