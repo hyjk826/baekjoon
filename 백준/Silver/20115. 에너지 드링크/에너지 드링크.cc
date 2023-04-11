116a4c5eac2ab40f9cd1e28c02058b21e48872f9
hyjk826 <hyjk826@naver.com>
2023-04-11 12:24:46 +0900

    [Silver III] Title: 에너지 드링크, Time: 20 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/20115. 에너지 드링크/에너지 드링크.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main(){
+	fastio;
+    int n;
+    cin >> n;
+    vi vec(n);
+    for(int i{0}; i < n; ++i){
+        cin >> vec[i];
+    }
+    sort(vec.begin(), vec.end());
+    ll sum = accumulate(vec.begin(), vec.begin() + n - 1, 0LL);
+    cout << vec[n - 1] + 1.0 * sum / 2;
+}
