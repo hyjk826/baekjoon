cac9dcc8e4f9a5f1a768ab0fbf205b616234dc6c
hyjk826 <hyjk826@naver.com>
2023-04-14 19:47:29 +0900

    [Bronze I] Title: 기상청 인턴 신현수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2435. 기상청 인턴 신현수/기상청 인턴 신현수.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main(){
+	fastio;
+    int n, k;
+    cin >> n >> k;
+    vl vec(n + 1);
+    for(int i{1}; i <= n; ++i){
+        cin >> vec[i];
+        vec[i] += vec[i - 1];
+    }
+    ll mx{-MAX};
+    for(int i{0}; i + k <= n; ++i){
+        mx = max(mx, vec[i + k] - vec[i]);
+    }
+    cout << mx;
+}
