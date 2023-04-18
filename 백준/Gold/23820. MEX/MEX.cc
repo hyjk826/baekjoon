bf1203fe498a82379b26c12644f8d919eb7df688
hyjk826 <hyjk826@naver.com>
2023-04-18 21:16:07 +0900

    [Gold II] Title: MEX, Time: 492 ms, Memory: 30060 KB -BaekjoonHub

---
+++ 백준/Gold/23820. MEX/MEX.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int ch[2000004];
+int ch2[2000004];
+
+int main(){
+	fastio;
+    int n;
+    cin >> n;
+    for(int i{0}; i < n; ++i){
+        int a;
+        cin >> a;
+        ch[a] = 1;
+    }
+    vi A;
+    for(int i{0}; i <= 2000003; ++i){
+        if(ch[i]) A.push_back(i);
+    }
+    for(auto& i : A){
+        for(ll j{0}; j < (int)A.size() && 1LL * i * A[j] <= 2000003; ++j){
+            ch2[i * A[j]] = 1;
+        }
+    }
+    for(int i{0}; i <= 2000003; ++i){
+        if(ch2[i]) continue;
+        cout << i; return 0;
+    }
+}
