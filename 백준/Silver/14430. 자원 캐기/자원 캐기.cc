d737c0a3568098f8a216418a3bdcafa5bcbe2393
hyjk826 <hyjk826@naver.com>
2023-04-21 17:32:42 +0900

    [Silver II] Title: 자원 캐기, Time: 4 ms, Memory: 2376 KB -BaekjoonHub

---
+++ 백준/Silver/14430. 자원 캐기/자원 캐기.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int board[301][301];
+
+int main(){
+	fastio;
+    int n, m;
+    cin >> n >> m;
+    for(int i{1}; i <= n; ++i){
+        for(int j{1}; j <= m; ++j){
+            cin >> board[i][j];
+            board[i][j] += max(board[i][j - 1], board[i - 1][j]);
+        }
+    }
+    cout << board[n][m];
+}
