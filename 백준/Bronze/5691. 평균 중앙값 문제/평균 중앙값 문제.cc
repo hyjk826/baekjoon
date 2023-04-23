5368dcd794ecee730ce6c84974d7d45ec1d00066
hyjk826 <hyjk826@naver.com>
2023-04-23 20:22:08 +0900

    [Bronze III] Title: 평균 중앙값 문제, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5691. 평균 중앙값 문제/평균 중앙값 문제.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main(){
+	fastio;
+    int a, b;
+    while(cin >> a >> b){
+        if(a == 0 && b == 0) break;
+        cout << 2 * a - b << "\n";
+    }
+}
