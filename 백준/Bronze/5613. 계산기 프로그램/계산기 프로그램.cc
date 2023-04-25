cc37ee675706453d645e91f5fff0643e22884ab9
hyjk826 <hyjk826@naver.com>
2023-04-25 11:46:55 +0900

    [Bronze III] Title: 계산기 프로그램, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/5613. 계산기 프로그램/계산기 프로그램.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main(){
+	fastio;
+    int a;
+    cin >> a;
+    string str;
+    int b;
+    while(1){
+        cin >> str;
+        if(str == "=") break;
+        int b;
+        cin >> b;
+        if(str == "+") a += b;
+        else if(str == "-") a -= b;
+        else if(str == "*") a *= b;
+        else a /= b;
+    }
+    cout << a;
+}
