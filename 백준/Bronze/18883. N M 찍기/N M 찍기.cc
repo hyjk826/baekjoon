7c4860dadabffce7504b7e7a05eafc9cec9d1933
hyjk826 <hyjk826@naver.com>
2023-04-29 00:16:52 +0900

    [Bronze III] Title: N M 찍기, Time: 100 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18883. N M 찍기/N M 찍기.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main(){
+	fastio;
+    int n, m;
+    cin >> n >> m;
+    for(int i{1}; i <= n * m; ++i){
+        cout << i;
+        if(i % m) cout << " ";
+        else cout << "\n";
+    }
+}
