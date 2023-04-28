02dbceee1da20340534c49db56f7476adc5c6eec
hyjk826 <hyjk826@naver.com>
2023-04-28 22:42:57 +0900

    [Bronze II] Title: 양념 반 후라이드 반, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16917. 양념 반 후라이드 반/양념 반 후라이드 반.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main(){
+    ll a, b, c, x, y;
+    cin >> a >> b >> c >> x >> y;
+    ll k = min(x, y) * 2 * c;
+    k += max(0LL, x - min(x, y)) * min(2 * c, a);
+    k += max(0LL, y - min(x, y)) * min(2 * c, b);
+    cout << min(a * x + b * y, k);
+}
