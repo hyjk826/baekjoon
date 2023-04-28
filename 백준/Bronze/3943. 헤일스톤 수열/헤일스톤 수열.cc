1988ae97457900c109b7cc60e375fe2ec12294bd
hyjk826 <hyjk826@naver.com>
2023-04-28 23:27:35 +0900

    [Bronze II] Title: 헤일스톤 수열, Time: 40 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3943. 헤일스톤 수열/헤일스톤 수열.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main(){
+    fastio;
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        int ans{n};
+        while(n != 1){
+            if(n & 1) n = n * 3 + 1;
+            else n /= 2;            
+            ans = max(ans, n);
+        }
+        cout << ans << "\n";
+    }
+}
