ccfe7dff39680279db409244714f38a6b13a86d8
hyjk826 <hyjk826@naver.com>
2023-04-29 00:08:02 +0900

    [Gold I] Title: 구슬 탈출 4, Time: 0 ms, Memory: 2064 KB -BaekjoonHub

---
+++ 백준/Gold/15653. 구슬 탈출 4/구슬 탈출 4.cc
@@ -0,0 +1,113 @@
+#include <iostream>
+#include <queue>
+#include <string>
+#include <stack>
+#include <map>
+#include <algorithm>
+#include <set>
+#include <cmath>
+#include <unordered_set>
+#include <map>
+#include <cstring>
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0);
+#define vi vector<int>
+#define pi pair<int, int>
+#define ll long long
+#define MAX 2147000000
+#define MOD 998244353LL
+using namespace std;
+
+int xx[]{ -1,0,1,0 };
+int yy[]{ 0,1,0,-1 };
+
+struct edge {
+	int rx, ry, bx, by, cnt;
+};
+int visit[10][10][10][10]{};
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int n, m;
+	cin >> n >> m;
+	edge s;
+	s.cnt = 0;
+	queue<edge> Q;
+	vector<vector<char> > map(n, vector<char>(m));
+	for (int i{ 0 }; i < n; ++i) {
+		for (int j{ 0 }; j < m; ++j) {
+			cin >> map[i][j];
+			if (map[i][j] == 'R') {
+				s.rx = i;
+				s.ry = j;
+				map[i][j] = '.';
+			}
+			else if (map[i][j] == 'B') {
+				s.bx = i;
+				s.by = j;
+				map[i][j] = '.';
+			}
+		}
+	}
+	Q.push(s);
+	visit[s.rx][s.ry][s.bx][s.by] = 1;
+	int ans{ -1 };
+	while (!Q.empty()) {
+		edge x = Q.front();
+		Q.pop();
+		if (map[x.bx][x.by] == 'O') continue;
+		if (map[x.rx][x.ry] == 'O' && map[x.bx][x.by] != 'O') {
+			ans = x.cnt;
+			break;
+		}
+		for (int i{ 0 }; i < 4; ++i) {
+			int nrx{ x.rx };
+			int nry{ x.ry };
+			int nbx{ x.bx };
+			int nby{ x.by };
+			while (1) {
+				nrx += xx[i];
+				nry += yy[i];
+				if (map[nrx][nry] == '#') {
+					nrx -= xx[i];
+					nry -= yy[i];
+					break;
+				}
+				else if (map[nrx][nry] == 'O') {
+					break;
+				}
+			}
+			while (1) {
+				nbx += xx[i];
+				nby += yy[i];
+				if (map[nbx][nby] == '#') {
+					nbx -= xx[i];
+					nby -= yy[i];
+					break;
+				}
+				else if (map[nbx][nby] == 'O') {
+					break;
+				}
+			}
+			if (nrx == nbx && nry == nby) {
+				if (map[nrx][nry] != 'O') {
+					int rm = abs(nrx - x.rx) + abs(nry - x.ry);
+					int bm = abs(nbx - x.bx) + abs(nby - x.by);
+					if (rm > bm) {
+						nrx -= xx[i];
+						nry -= yy[i];
+					}
+					else {
+						nbx -= xx[i];
+						nby -= yy[i];
+					}
+				}
+			}
+			if (visit[nrx][nry][nbx][nby] == 0) {
+				visit[nrx][nry][nbx][nby] = 1;
+				Q.push({ nrx,nry,nbx,nby,x.cnt + 1 });
+			}
+		}
+	}
+	cout << ans;
+}
