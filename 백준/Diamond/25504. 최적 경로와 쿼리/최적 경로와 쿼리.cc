f3c8b0e078ffe97b403375b59e34ca3ca2bbe570
hyjk826 <hyjk826@naver.com>
2023-04-29 23:12:51 +0900

    [Diamond V] Title: 최적 경로와 쿼리, Time: 1232 ms, Memory: 129308 KB -BaekjoonHub

---
+++ 백준/Diamond/25504. 최적 경로와 쿼리/최적 경로와 쿼리.cc
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+vector<vp> g(50001);
+vector<vi> dist(50001);
+
+
+int main(){
+	fastio;
+    int n, m, q;
+    cin >> n >> m >> q;
+    vi degree(n + 1);
+    for(int i{0}; i < m; ++i){
+        int a, b, c;
+        cin >> a >> b >> c;
+        g[a].push_back({b, c});
+        g[b].push_back({a, c});
+    }
+    for(int i{1}; i <= n; ++i){
+        sort(g[i].begin(), g[i].end());
+    }
+    for(int i{1}; i <= n; ++i){
+        if(g[i].size() >= 80){
+            dist[i].resize(n + 1, MAX);
+            dist[i][i] = 0;
+            for(auto& j : g[i]){
+                dist[i][j.first] = min(dist[i][j.first], j.second);
+                for(auto& k : g[j.first]){
+                    dist[i][k.first] = min(dist[i][k.first], j.second + k.second);
+                }
+            }
+        }
+    }
+    while(q--){
+        int a, b;
+        cin >> a >> b;
+        if(g[a].size() < g[b].size()) swap(a, b);
+        int ans{MAX};
+        if(g[a].size() >= 80){            
+            for(auto& i : g[b]){
+                ans = min(ans, dist[a][i.first] + i.second);
+            }
+        }
+        else{
+            for(auto& i : g[a]){
+                if(i.first == b) ans = min(ans, i.second);
+                for(auto& j : g[b]){
+                    if(i.first == j.first) ans = min(ans, i.second + j.second);
+                    auto it = lower_bound(g[i.first].begin(), g[i.first].end(), make_pair(j.first, 0));
+                    if(it != g[i.first].end() && it->first == j.first){
+                        ans = min(ans, i.second + j.second + it->second);
+                    }
+                }
+            }
+        }
+        if(ans == MAX) cout << -1 << "\n";
+        else cout << ans << "\n";
+    }
+}
