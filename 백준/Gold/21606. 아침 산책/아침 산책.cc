4e945d7dac18ff45254f698df144bb22a350506b
hyjk826 <hyjk826@naver.com>
2023-05-02 16:52:59 +0900

    [Gold III] Title: 아침 산책, Time: 104 ms, Memory: 15992 KB -BaekjoonHub

--- 백준/Gold/21606. 아침 산책/아침 산책.cc
+++ 백준/Gold/21606. 아침 산책/아침 산책.cc
@@ -22,27 +22,36 @@ int main(){
     string str;
     cin >> str;
     vector<vi> g(n + 1);
+    ll ans{0};
     for(int i{0}; i < n - 1; ++i){
         int a, b;
         cin >> a >> b;
+        if(str[a - 1] == str[b - 1] && str[a - 1] == '1') ans += 2;
         g[a].push_back(b);
         g[b].push_back(a);
     }
-    vi dp(n + 1);
-    ll ans{0};
-    function<void(int,int)> f = [&](int cur, int pre){
-        if(str[cur - 1] == '1') dp[cur] = 1;
-        int sum{0};
-        for(auto& i : g[cur]){
-            if(i == pre) continue;
-            f(i, cur);
-            if(str[cur - 1] == '1') ans += dp[i];
-            else {
-                ans += 1LL * dp[i] * dp[cur];
-                dp[cur] += dp[i];
+    vi ch(n + 1);
+    for(int i{1}; i <= n; ++i){
+        if(ch[i]) continue;
+        if(str[i - 1] == '1') continue;
+        queue<int> Q;
+        Q.push(i);
+        ch[i] = 1;
+        int cnt{0};
+        while(!Q.empty()){
+            int x{Q.front()};
+            Q.pop();
+            for(auto& j : g[x]){
+                if(str[j - 1] == '1') cnt++;
+                else{
+                    if(ch[j] == 0){
+                        ch[j] = 1;
+                        Q.push(j);
+                    }
+                }
             }
         }
-    };
-    f(1, -1);
-    cout << ans * 2;
+        ans += 1LL * cnt * (cnt - 1);
+    }
+    cout << ans;
 }
