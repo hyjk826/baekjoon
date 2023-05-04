91bfc1971a06a366917c359ba2f06012d31f4e3b
hyjk826 <hyjk826@naver.com>
2023-05-05 07:11:14 +0900

    [Silver I] Title: 1, 2, 3 더하기 7, Time: 40 ms, Memory: 41124 KB -BaekjoonHub

---
+++ 백준/Silver/15992. 1， 2， 3 더하기 7/1， 2， 3 더하기 7.cc
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000009
+using namespace std;
+
+int dp[10001][1001];
+
+int main(){
+	fastio;
+    memset(dp, -1, sizeof(dp));
+    function<int(int,int)> f = [&](int a, int b){
+        if(a <= 0 || a < b) return 0;
+        if(dp[a][b] != -1) return dp[a][b];
+        int& ret = dp[a][b];
+        ret = 0;
+        if(b == 1){
+            if(a == 1 || a == 2 || a == 3) ret = 1;
+            else ret = 0;
+        }
+        else{
+            for(int i{1}; i <= 3; ++i){
+                ret += f(a - i, b - 1);
+                ret %= MOD;
+            }            
+        }
+        return ret;
+    };
+    int q;
+    cin >> q;
+    while(q--){
+        int a, b;
+        cin >> a >> b;
+        cout << f(a, b) << "\n";
+    }
+}
