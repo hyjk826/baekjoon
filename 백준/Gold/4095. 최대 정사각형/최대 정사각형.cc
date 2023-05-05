f76df992f235dc390fe6b2fad60d6a8e6f3bfce6
hyjk826 <hyjk826@naver.com>
2023-05-05 21:17:08 +0900

    [Gold IV] Title: 최대 정사각형, Time: 848 ms, Memory: 5996 KB -BaekjoonHub

---
+++ 백준/Gold/4095. 최대 정사각형/최대 정사각형.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main(){
+	fastio;
+    int n, m;
+    while(cin >> n >> m){
+        if(n == 0) break;
+        vector<vi> board(n + 1, vi(m + 1));
+        int ans{0};
+        for(int i{1}; i <= n; ++i){
+            for(int j{1}; j <= m; ++j){
+                cin >> board[i][j];
+                if(board[i][j]){
+                    board[i][j] = min({board[i][j - 1], board[i - 1][j], board[i - 1][j - 1]}) + 1;
+                }
+                ans = max(ans, board[i][j]);
+            }
+        }
+        cout << ans << "\n";
+    }
+}
