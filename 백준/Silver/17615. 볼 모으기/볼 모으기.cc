0c1aed9806dfc40d5b85a94fdade8db6d680b67d
hyjk826 <hyjk826@naver.com>
2023-05-08 05:50:20 +0900

    [Silver I] Title: 볼 모으기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/17615. 볼 모으기/볼 모으기.cc
+++ 백준/Silver/17615. 볼 모으기/볼 모으기.cc
@@ -1,4 +1,3 @@
-// 2022-09-08
 #include <bits/stdc++.h>
 #define fastio                    \
 	ios_base::sync_with_stdio(0); \
@@ -10,39 +9,24 @@
 #define pi pair<int, int>
 #define pl pair<ll, ll>
 #define vp vector<pi>
+#define vpl vector<pl>
 #define ll long long
 #define MAX 2147000000
 #define MOD 1000000007
 using namespace std;
 
-int main() {
+int main(){
 	fastio;
-    int n;
+	int n;
     cin >> n;
     string str;
     cin >> str;
-    vp vec;
-    vec.push_back({str[0] - 'A', 1});
-    for(int i{1}; i < (int)str.size(); ++i){
-        if(vec.back().first == str[i] - 'A') vec.back().second++;
-        else vec.push_back({str[i] - 'A', 1});
-    }
+    string temp = str;
+    while(!temp.empty() && temp.back() == 'R') temp.pop_back();
     int ans{MAX};
-    int a{0}, b{0};
-    for(int i{0}; i < (int)vec.size() - 1; ++i){
-        if(i & 1) a += vec[i].second;
-        else b += vec[i].second;
-    }
-    ans = min(ans, a);
-    ans = min(ans, b);
-    a = b = 0;
-    for(int i{1}; i < (int)vec.size(); ++i){
-        if(i & 1) a+= vec[i].second;
-        else b += vec[i].second;
-    }
-    ans = min(ans, a);
-    ans = min(ans, b);
+    ans = min(ans, (int)count(temp.begin(), temp.end(), 'R'));
+    temp = str;
+    while(!temp.empty() && temp.back() == 'B') temp.pop_back();
+    ans = min(ans, (int)count(temp.begin(), temp.end(), 'B'));
     cout << ans;
 }
-	
-
