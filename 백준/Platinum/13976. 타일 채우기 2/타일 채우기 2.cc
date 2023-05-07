5818a05abf7614f0fffe97fdacc1cd29674b791f
hyjk826 <hyjk826@naver.com>
2023-05-08 07:50:08 +0900

    [Platinum V] Title: 타일 채우기 2, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Platinum/13976. 타일 채우기 2/타일 채우기 2.cc
@@ -0,0 +1,119 @@
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+const int mod = 1000000007;
+ll ipow(ll x, ll p){
+	ll ret = 1, piv = x;
+	while(p){
+		if(p & 1) ret = ret * piv % mod;
+		piv = piv * piv % mod;
+		p >>= 1;
+	}
+	return ret;
+}
+
+vector<int> berlekamp_massey(vector<int> x){
+	vector<int> ls, cur;
+	int lf, ld;
+	for(int i=0; i<x.size(); i++){
+		ll t = 0;
+		for(int j=0; j<cur.size(); j++){
+			t = (t + 1ll * x[i-j-1] * cur[j]) % mod;
+		}
+		if((t - x[i]) % mod == 0) continue;
+		if(cur.empty()){
+			cur.resize(i+1);
+			lf = i;
+			ld = (t - x[i]) % mod;
+			continue;
+		}
+		ll k = -(x[i] - t) * ipow(ld, mod - 2) % mod;
+		vector<int> c(i-lf-1);
+		c.push_back(k);
+		for(auto &j : ls) c.push_back(-j * k % mod);
+		if(c.size() < cur.size()) c.resize(cur.size());
+		for(int j=0; j<cur.size(); j++){
+			c[j] = (c[j] + cur[j]) % mod;
+		}
+		if(i-lf+(int)ls.size()>=(int)cur.size()){
+			tie(ls, lf, ld) = make_tuple(cur, i, (t - x[i]) % mod);
+		}
+		cur = c;
+	}
+	for(auto &i : cur) i = (i % mod + mod) % mod;
+	return cur;
+}
+int get_nth(vector<int> rec, vector<int> dp, ll n){
+	int m = rec.size();
+	vector<int> s(m), t(m);
+	s[0] = 1;
+	if(m != 1) t[1] = 1;
+	else t[0] = rec[0];
+	auto mul = [&rec](vector<int> v, vector<int> w){
+		int m = v.size();
+		vector<int> t(2 * m);
+		for(int j=0; j<m; j++){
+			for(int k=0; k<m; k++){
+				t[j+k] += 1ll * v[j] * w[k] % mod;
+				if(t[j+k] >= mod) t[j+k] -= mod;
+			}
+		}
+		for(int j=2*m-1; j>=m; j--){
+			for(int k=1; k<=m; k++){
+				t[j-k] += 1ll * t[j] * rec[k-1] % mod;
+				if(t[j-k] >= mod) t[j-k] -= mod;
+			}
+		}
+		t.resize(m);
+		return t;
+	};
+	while(n){
+		if(n & 1) s = mul(s, t);
+		t = mul(t, t);
+		n >>= 1;
+	}
+	ll ret = 0;
+	for(int i=0; i<m; i++) ret += 1ll * s[i] * dp[i] % mod;
+	return ret % mod;
+}
+int guess_nth_term(vector<int> x, ll n){
+	if(n < x.size()) return x[n];
+	vector<int> v = berlekamp_massey(x);
+	if(v.empty()) return 0;
+	return get_nth(v, x, n);
+}
+
+int main() {
+	fastio;
+	ll n;
+	cin >> n;
+	vi dp(50);
+	dp[0] = 1;
+	dp[1] = 0;
+	dp[2] = 3;
+	dp[3] = 0;
+	for (int i{ 4 }; i <= 49; ++i) {
+		ll k = 1LL * 3 * dp[i - 2];
+        k %= MOD;
+		for (int j{ 4 }; j <= i; j += 2) {
+			k += 1LL * dp[i - j] * 2;
+            k %= MOD;
+		}
+        dp[i] = k;
+	}
+    cout << guess_nth_term(dp, n);
+}
