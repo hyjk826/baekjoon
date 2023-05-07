dd9bca8f0f67216d4fa6a53840c1ff06da8c61be
hyjk826 <hyjk826@naver.com>
2023-05-08 06:11:54 +0900

    [Silver V] Title: 귀찮음, Time: 52 ms, Memory: 3924 KB -BaekjoonHub

---
+++ 백준/Silver/16208. 귀찮음/귀찮음.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main(){
+	fastio;
+	int n;
+    cin >> n;
+    vi vec(n);
+    ll sum{0};
+    for(int i{0}; i < n; ++i){
+        cin >> vec[i];
+        sum += vec[i];
+    }
+    ll ans{0};
+    sort(vec.rbegin(), vec.rend());
+    for(int i{0}; i < n; ++i){
+        sum -= vec[i];
+        ans += vec[i] * sum;
+    }
+    cout << ans;
+}
