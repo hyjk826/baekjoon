dc848ec1f3a653554674b3de110c432c3b2c13ff
hyjk826 <hyjk826@naver.com>
2023-05-08 06:08:18 +0900

    [Silver III] Title: 모두의 마블, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/12845. 모두의 마블/모두의 마블.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int main(){
+	fastio;
+	int n;
+    cin >> n;
+    vi vec(n);
+    for(int i{0}; i < n; ++i){
+        cin >> vec[i];
+    }
+    int mx = *max_element(vec.begin(), vec.end());
+    int sum = accumulate(vec.begin(), vec.end(), 0);
+    cout << mx * (n - 1) + (sum - mx);
+}
