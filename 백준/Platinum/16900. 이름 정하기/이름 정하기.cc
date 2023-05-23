30dad6dfea266688c94963db1479dbe78e0a7f42
hyjk826 <hyjk826@naver.com>
2023-05-24 00:24:49 +0900

    [Platinum V] Title: 이름 정하기, Time: 4 ms, Memory: 5104 KB -BaekjoonHub

---
+++ 백준/Platinum/16900. 이름 정하기/이름 정하기.cc
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+int table(string s) {
+	int m{ (int)s.size() };
+	vector<int> t(m);
+	int j{ 0 };
+	for (int i{ 1 }; i < m; ++i) {
+		while (j && s[i] != s[j]) j = t[j - 1];
+		if (s[i] == s[j]) t[i] = ++j;
+	}
+	return t.back();
+}
+
+
+int main(){
+	fastio;
+	string str;
+    int k;
+    cin >> str >> k;
+    int a = table(str);
+    a = (int)str.size() - a;
+    cout << 1LL * a * (k - 1) + (int)str.size();
+}
