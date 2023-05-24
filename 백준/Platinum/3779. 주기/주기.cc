6202f4566eab6e616a97e83f6c022eb0e686e8f7
hyjk826 <hyjk826@naver.com>
2023-05-25 01:08:26 +0900

    [Platinum IV] Title: 주기, Time: 44 ms, Memory: 8984 KB -BaekjoonHub

---
+++ 백준/Platinum/3779. 주기/주기.cc
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+vi table(string s) {
+	int m{ (int)s.size() };
+	vector<int> t(m);
+	int j{ 0 };
+	for (int i{ 1 }; i < m; ++i) {
+		while (j && s[i] != s[j]) j = t[j - 1];
+		if (s[i] == s[j]) t[i] = ++j;
+	}
+	return t;
+}
+
+
+int main(){
+	fastio;
+    for(int T{1};;++T){
+        int n;
+        cin >> n;
+        if(n == 0) break;
+        cout << "Test case #" << T << "\n";
+        string str;
+        cin >> str;
+        auto A = table(str);
+        for(int i{1}; i < (int)str.size(); ++i){
+            if(A[i] == 0) continue;
+            if((i + 1) % (i + 1 - A[i]) == 0){
+                cout << i + 1 << " " << (i + 1) / (i + 1 - A[i]) << "\n";
+            }
+        }
+        cout << "\n";
+    }
+}
