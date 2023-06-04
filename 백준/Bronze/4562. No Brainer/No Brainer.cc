11ca0056a802459884b02f86b7256b2e32adc480
hyjk826 <hyjk826@naver.com>
2023-06-05 04:50:01 +0900

    [Bronze III] Title: No Brainer, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4562. No Brainer/No Brainer.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+void solve(){
+    int a, b;
+    cin >> a >> b;
+    if(a >= b) cout << "MMM BRAINS\n";
+    else cout << "NO BRAINS\n";
+}
+
+int main(){
+	fastio;
+	int T;
+	cin >> T;
+	while(T--){
+		solve();
+	}
+}
