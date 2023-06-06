0dc8f2edd851c9ba6688b366ccb0096523370a40
hyjk826 <hyjk826@naver.com>
2023-06-06 23:55:31 +0900

    [Bronze V] Title: Mathematics, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26545. Mathematics/Mathematics.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+void solve(){
+    int n;
+    cin >> n;
+    vi vec(n);
+    for(auto& i : vec) cin >> i;
+    cout << accumulate(vec.begin(), vec.end(), 0LL);
+}
+
+int main(){
+	fastio;
+	int T;
+	T = 1;
+	while(T--){
+		solve();
+	}
+}
