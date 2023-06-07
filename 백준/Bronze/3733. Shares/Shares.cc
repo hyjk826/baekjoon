47fd09e9b9c227ff0cf7877ef526e33a1cada70e
hyjk826 <hyjk826@naver.com>
2023-06-07 20:59:08 +0900

    [Bronze V] Title: Shares, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3733. Shares/Shares.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+void solve(){
+
+}
+
+int main(){
+	fastio;
+    int a, b;
+	while(cin >> a >> b){
+        cout << b / (a + 1) << "\n";
+    }
+}
