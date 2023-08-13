31f06faba83b4828d909ac8eb806ecdec116c443
hyjk826 <hyjk826@naver.com>
2023-08-14 00:46:13 +0900

    [Platinum V] Title: 주유소, Time: 408 ms, Memory: 57476 KB -BaekjoonHub

--- 백준/Platinum/13308. 주유소/주유소.cc
+++ 백준/Platinum/13308. 주유소/주유소.cc
@@ -1,4 +1,3 @@
-// 2022-11-14
 #include <bits/stdc++.h>
 #define fastio                    \
 	ios_base::sync_with_stdio(0); \
@@ -10,57 +9,69 @@
 #define pi pair<int, int>
 #define pl pair<ll, ll>
 #define vp vector<pi>
+#define vpl vector<pl>
 #define ll long long
 #define MAX 2147000000
 #define MOD 1000000007
 using namespace std;
 
 struct st{
-    ll v, w, c;
-    bool operator< (const st a) const{
+    int v, c;
+    ll w;
+    bool operator< (const st& a) const{
         return w > a.w;
     }
 };
 
-
-int main() {
-	fastio;
-    int n, m;
+void solve(){
+	int n, m;
     cin >> n >> m;
-    vl cost(n + 1);
-    for(int i{1}; i <= n; ++i){
-        cin >> cost[i];
-    }
-    vector<vector<pl>> g(n + 1);
+    vi cost(n + 1);
+    for(int i{1}; i <= n; ++i) cin >> cost[i];
+    vector<vp> g(n + 1);
     for(int i{0}; i < m; ++i){
-        ll a, b, c;
+        int a, b, c;
         cin >> a >> b >> c;
         g[a].push_back({b, c});
         g[b].push_back({a, c});
     }
-    priority_queue<st> pQ;
-    pQ.push({1, 0, cost[1]});
     vector<vl> dijk(n + 1, vl(2501, LLONG_MAX));
+    priority_queue<st> pQ;
+    pQ.push({1, cost[1], 0});
+    dijk[1][cost[1]] = 0;
+    vi ch(n + 1, -1);
+    ch[1] = cost[1];
     while(!pQ.empty()){
-        ll v{pQ.top().v};
+        int v{pQ.top().v};
+        int c{pQ.top().c};
         ll w{pQ.top().w};
-        ll c{pQ.top().c};
         pQ.pop();
-        if(v == n){
-            cout << w;
-            return 0;
-        }
         if(dijk[v][c] < w) continue;
+        if(cost[v] < c) continue;
+        cost[v] = c;
         for(auto& i : g[v]){
-            ll nv{i.first};
-            ll k = min(cost[v], c);
-            ll nw{w + k * i.second};
-            if(dijk[nv][k] > nw){
-                dijk[nv][k] = nw;
-                pQ.push({nv, nw, k});
+            int nv{i.first};
+            ll nw{i.second * c + w};
+            int nc = min(c, cost[nv]);
+            if(dijk[nv][nc] > nw){
+                dijk[nv][nc] = nw;
+                pQ.push({nv, nc, nw});
             }
-        }
+        } 
     }
+    ll ans{LLONG_MAX};
+    for(int i{1}; i <= 2500; ++i){
+        ans = min(ans, dijk[n][i]);
+    }
+    cout << ans << "\n";
 }
-	
 
+
+int main(){
+	fastio;
+	int T;
+	T = 1;
+	while(T--){
+		solve();
+	}
+}
