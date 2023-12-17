acee100b0a6b226d0875f2a132f28933b08e3f25
hyjk826 <hyjk826@naver.com>
2023-12-17 20:01:52 +0900

    [Gold IV] Title: 최솟값, 최댓값, Time: 192 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Gold/30961. 최솟값， 최댓값/최솟값， 최댓값.cc
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define MAX 2147000000
+#define MOD 1000000007
+typedef long long ll;
+using namespace std;
+
+void solve(){
+	int n;
+    cin >> n;
+    vi vec(n);
+    for(int i{0}; i < n; ++i){
+        cin >> vec[i];
+    }
+    sort(vec.begin(), vec.end());
+    ll ans{0};
+    for(int i{0}; i < n; ++i){
+        if(i) ans ^= 1LL * vec[i] * vec[i - 1];
+        ans ^= 1LL * vec[i] * vec[i];
+    }
+    cout << ans << "\n";
+}
+
+int main(){
+	fastio;
+	int T;
+	T = 1;
+	while(T--){
+		solve();
+	}
+}
