c7991b3634500c0c86c408b65f8d0d842c2fc2d6
hyjk826 <hyjk826@naver.com>
2024-01-07 16:45:29 +0900

    [Bronze I] Title: 랜덤 게임?, Time: 188 ms, Memory: 30352 KB -BaekjoonHub

---
+++ 백준/Bronze/27865. 랜덤 게임？/랜덤 게임？.cc
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define MAX 2147000000
+#define MOD 1000000007
+typedef long long ll;
+using namespace std;
+
+void solve(){
+	int n;
+    cin >> n;
+    while(1){
+        cout << "? 1" << endl;
+        char c;
+        cin >> c;
+        if(c == 'Y'){
+            cout <<"! 1" << endl;
+            return;
+        }
+    }
+}
+
+int main(){
+	fastio;
+	int T;
+	T = 1;
+	while(T--){
+		solve();
+	}
+}
