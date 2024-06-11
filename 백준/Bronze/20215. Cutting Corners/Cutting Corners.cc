e4e35e01fa13f3d87c792d961b1578848866973e
hyjk826 <hyjk826@naver.com>
2024-06-11 14:15:06 +0900

    [Bronze IV] Title: Cutting Corners, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/20215. Cutting Corners/Cutting Corners.cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define vp vector<pi>
+#define vpl vector<pl>
+#define ll long long
+#define MAX 2147000000
+#define MOD 1000000007
+using namespace std;
+
+void solve(){
+    cout << fixed << setprecision(10);
+    int w, h;
+    cin >> w >> h;
+    cout << w + h - sqrt(w * w + h * h) << "\n";
+}
+
+int main(){
+	ios_base::sync_with_stdio(0); 
+	cin.tie(0);
+	int T;
+	T = 1;
+	while(T--){
+		solve();
+	}
+}
