42d4d69bad1cfcd4daa0d8b7552bf20b41a30b5c
hyjk826 <hyjk826@naver.com>
2024-07-12 22:53:12 +0900

    [Platinum IV] Title: 경찰차, Time: 40 ms, Memory: 9956 KB -BaekjoonHub

--- 백준/Platinum/2618. 경찰차/경찰차.cc
+++ 백준/Platinum/2618. 경찰차/경찰차.cc
@@ -1,60 +1,76 @@
-// 2022-07-12
 #include <bits/stdc++.h>
-#define fastio                    \
-	ios_base::sync_with_stdio(0); \
-	cin.tie(0);
 #define vi vector<int>
 #define vl vector<long long>
 #define vc vector<char>
+#define vs vector<string>
 #define pi pair<int, int>
+#define pl pair<ll, ll>
 #define vp vector<pi>
+#define vpl vector<pl>
 #define ll long long
 #define MAX 2147000000
 #define MOD 1000000007
 using namespace std;
 
-vector<vi> dp(1001, vi(1001, -1));
-vector<vi> ch(1001, vi(1001, -1));
-int n, m;
-vp vec;
-
-int Dp(int a, int b){
-    if(a == m || b == m) return dp[a][b] = 0;
-    if(dp[a][b] != -1) return dp[a][b];
-    int nx = max(a, b) + 1;
-    int x, y;
-    if(a == 0){
-        x = Dp(nx, b) + abs(1 - vec[nx].first) + abs(1 - vec[nx].second);
-    }
-    else x = Dp(nx, b) + abs(vec[a].first - vec[nx].first) + abs(vec[a].second - vec[nx].second);
-    if(b == 0){
-        y = Dp(a, nx) + abs(vec[nx].first - n) + abs(vec[nx].second - n);
-    }
-    else y = Dp(a, nx) + abs(vec[b].first - vec[nx].first) + abs(vec[b].second - vec[nx].second);
-    if(x < y){
-        ch[a][b] = 1;
-    }
-    else ch[a][b] = 2;
-    return dp[a][b] = min(x, y);
-}
-
-int main() {
-	fastio;
+void solve(){
+	int n, m;
     cin >> n >> m;
-    vec.resize(m + 1);
+    vp vec(m + 1);
     for(int i{1}; i <= m; ++i){
         cin >> vec[i].first >> vec[i].second;
     }
-    cout << Dp(0, 0) << "\n";
-    int a = 0;
-    int b = 0;
-    while(1){
-        if(ch[a][b] == -1) break;
-        cout << ch[a][b] << "\n";
-        int nx = max(a, b) + 1;
-        if(ch[a][b] == 1){
-            a = nx;
+    vector<vi> dp(m + 1, vi(m + 1, -1));
+    vector<vi> ch(m + 1, vi(m + 1, -1));
+    function<int(int,int)> f = [&](int a, int b){
+        if(a == m || b == m) return 0;
+        if(dp[a][b] != -1) return dp[a][b];
+        int x1, y1, x2, y2;
+        if(a == 0) {
+            x1 = y1 = 1;
         }
-        else b = nx;
+        else{
+            x1 = vec[a].first; y1 = vec[a].second;
+        }
+        if(b == 0){
+            x2 = y2 = n;
+        }
+        else{
+            x2 = vec[b].first; y2 = vec[b].second;
+        }
+        int next = max(a, b) + 1;
+        int first = f(next, b) + abs(vec[next].first - x1) + abs(vec[next].second - y1);
+        int second = f(a, next) + abs(vec[next].first - x2) + abs(vec[next].second - y2);
+        if(first <= second){
+            ch[a][b] = 1;
+            dp[a][b] = first;
+        }
+        else{
+            ch[a][b] = 2;
+            dp[a][b] = second;
+        }
+        return dp[a][b];
+    };
+    cout << f(0, 0) << "\n";
+    int x, y;
+    x = y = 0;
+    while(x != m && y != m){
+        if(ch[x][y] == 1){
+            cout << 1 << "\n";
+            x = max(x, y) + 1;
+        }
+        else{
+            cout << 2 << "\n";
+            y = max(x, y) + 1;
+        }
     }
+}
+
+int main(){
+	ios_base::sync_with_stdio(0); 
+	cin.tie(0);
+	int T;
+	T = 1;
+	while(T--){
+		solve();
+	}
 }
