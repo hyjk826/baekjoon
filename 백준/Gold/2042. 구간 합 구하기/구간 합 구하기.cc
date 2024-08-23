7e8ceb8a1e843fbc7c623462f6e20ae2ab73b519
hyjk826 <hyjk826@naver.com>
2024-08-23 23:23:04 +0900

    [Gold I] Title: 구간 합 구하기, Time: 184 ms, Memory: 41084 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
@@ -1,24 +1,18 @@
-#include <bits/stdc++.h>
-#define fastio                    \
-	ios_base::sync_with_stdio(0); \
-	cin.tie(0);
-#define vi vector<int>
-#define vl vector<long long>
-#define vc vector<char>
-#define vs vector<string>
-#define pi pair<int, int>
-#define pl pair<ll, ll>
-#define vp vector<pi>
-#define vpl vector<pl>
-#define ll long long
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#define vi vector<int> 
+#define pi pair<int, int> 
+#define vp vector<pi> 
 #define MAX 2147000000
-#define MOD 1000000007
-using namespace std;
+#define ll long long 
+using namespace std; 
 
-int n, a, b;
 const int sz = (int)1e6 + 10;
-ll seg[sz * 4];
 ll arr[sz];
+ll seg[sz * 4];
+
 
 void init(int node, int l, int r){
     if(l == r) seg[node] = arr[l];
@@ -30,49 +24,47 @@ void init(int node, int l, int r){
     }
 }
 
-void update(int node, int l, int r, int idx, ll k){
-    if(idx < l || idx > r) return;
-    if(l == r) {
-        seg[node] = k;
+void update(int node, int l, int r, int idx, ll value){
+    if(idx < l || r < idx) return;
+    if(l == r){
+        seg[node] = arr[idx] = value;
     }
     else{
         int mid{(l + r) >> 1};
-        update(node << 1, l, mid, idx, k);
-        update(node << 1 | 1, mid + 1, r, idx, k);
+        update(node << 1, l, mid, idx, value);
+        update(node << 1 | 1, mid + 1, r, idx, value);
         seg[node] = seg[node << 1] + seg[node << 1 | 1];
     }
 }
 
-ll query(int node, int l, int r, int s, int e){
+ll sum(int node, int l, int r, int s, int e){
     if(r < s || e < l) return 0LL;
-    if(s <= l && r <= e) return (ll)seg[node];
+    if(s <= l && r <= e) return seg[node];
     int mid{(l + r) >> 1};
-    return query(node << 1, l, mid, s, e) + query(node << 1 | 1, mid + 1, r, s, e);
+    ll left = sum(node << 1, l, mid, s, e);
+    ll right = sum(node << 1 | 1, mid + 1, r, s, e);
+    return left + right;
 }
 
-void solve(){
-	cin >> n >> a >> b;
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    int n, m, k;
+    cin >> n >> m >> k;
     for(int i{1}; i <= n; ++i){
         cin >> arr[i];
     }
     init(1, 1, n);
-    for(int i{0}; i < a + b; ++i){
-        ll op, l, r;
-        cin >> op >> l >> r;
+
+    for(int i{0}; i < m + k; ++i){
+        ll op, a, b;
+        cin >> op >> a >> b;
         if(op == 1){
-            update(1, 1, n, l, r);
+            update(1, 1, n, a, b);
         }
         else{
-            cout << query(1, 1, n, l, r) << "\n";
+            cout << sum(1, 1, n, a, b) << "\n";
+            
         }
     }
-}
-
-int main(){
-	fastio;
-	int T;
-	T = 1;
-	while(T--){
-		solve();
-	}
 }
