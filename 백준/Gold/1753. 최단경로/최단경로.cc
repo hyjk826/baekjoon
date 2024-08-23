51d52ec5c5ed4a2f00db9734c6af16ca24b4a709
hyjk826 <hyjk826@naver.com>
2024-08-23 23:07:28 +0900

    [Gold IV] Title: 최단경로, Time: 84 ms, Memory: 9112 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.cc
+++ 백준/Gold/1753. 최단경로/최단경로.cc
@@ -1,58 +1,57 @@
-// 2022-09-11
-#include <bits/stdc++.h>
-#define fastio                    \
-	ios_base::sync_with_stdio(0); \
-	cin.tie(0);
-#define vi vector<int>
-#define vl vector<long long>
-#define vc vector<char>
-#define vs vector<string>
-#define pi pair<int, int>
-#define vp vector<pi>
-#define ll long long
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#define vi vector<int> 
+#define pi pair<int, int> 
+#define vp vector<pi> 
 #define MAX 2147000000
-#define MOD 1000000007
-using namespace std;
+#define ll long long 
+using namespace std; 
 
 struct st{
-    int v, dis;
-    bool operator< (const st a) const{
-        return dis > a.dis;
+    int v, w;
+    bool operator< (const st& a) const{
+        return w > a.w;
     }
 };
 
 int main(){
-    fastio;
-    int n, m;
-    cin >> n >> m;
-    int s;
-    cin >> s;
-    vector<vector<st>> g(n + 1);
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    int n, m, s;
+    cin >> n >> m >> s;
+
+    vector<vp> g(n + 1);
     for(int i{0}; i < m; ++i){
         int a, b, c;
         cin >> a >> b >> c;
         g[a].push_back({b, c});
     }
-    vi dijk(n + 1, (int)1e9);
-    dijk[s] = 0;
+
     priority_queue<st> pQ;
+
     pQ.push({s, 0});
+
+    vi dist(n + 1, MAX);
+    dist[s] = 0;
+
     while(!pQ.empty()){
         int v{pQ.top().v};
-        int dis{pQ.top().dis};
+        int w{pQ.top().w};
         pQ.pop();
-        if(dis > dijk[v]) continue;
+        if(dist[v] < w) continue;
         for(auto& i : g[v]){
-            int nv{i.v};
-            int ndis{dis + i.dis};
-            if(dijk[nv] > ndis){
-                dijk[nv] = ndis;
-                pQ.push({nv, dijk[nv]});
+            int nv{i.first};
+            int nw{w + i.second};
+            if(dist[nv] > nw){
+                dist[nv] = nw;
+                pQ.push({nv, nw});
             }
         }
     }
+
     for(int i{1}; i <= n; ++i){
-        if(dijk[i] == (int)1e9) cout << "INF\n";
-        else cout << dijk[i] << "\n";
+        if(dist[i] == MAX) cout << "INF\n";
+        else cout << dist[i] << "\n";
     }
 }
